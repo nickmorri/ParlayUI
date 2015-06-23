1a455e6cfdacf150386d7d0be793fa14e140a7c5
Nicholas Morris <nick@promenadesoftware.com>
2015-06-23 09:20:52 -0700

    fix: Fixed package.JSON type

--- Gruntfile.js
+++ Gruntfile.js
@@ -4,7 +4,7 @@ module.exports = function (grunt) {
   require('load-grunt-tasks')(grunt);
 
   grunt.initConfig({
-    'pkg': grunt.file.readJSON('package.JSON'),
+    'pkg': grunt.file.readJSON('package.json'),
 
     'meta': {
       'source': [

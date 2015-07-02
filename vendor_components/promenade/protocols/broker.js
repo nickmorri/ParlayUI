55c7166e28f46dbcc05bd2f21c096ddb6706d6c3
Nick Morris <nick@promenadesoftware.com>
2015-07-02 16:29:59 -0700

    feat: Adding broker transaction controls

--- vendor_components/promenade/protocols/broker.js
+++ vendor_components/promenade/protocols/broker.js
@@ -8,6 +8,7 @@ broker.factory('PromenadeBroker', ['ParlaySocket', '$q', function (ParlaySocket,
     };
     
     /**
+     * Requests the ParlaySocket to open itself.
      * @returns {$q.defer.promise} Requests the ParlaySocket to open.
      */
     Public.connect = function () {
@@ -15,6 +16,7 @@ broker.factory('PromenadeBroker', ['ParlaySocket', '$q', function (ParlaySocket,
     };
     
     /**
+     * Requests the ParlaySocket to close itself.
      * @returns {$q.defer.promise} Requests the ParlaySocket to close.
      */
     Public.disconnect = function () {
@@ -22,29 +24,72 @@ broker.factory('PromenadeBroker', ['ParlaySocket', '$q', function (ParlaySocket,
     };
     
     /**
+     * Check status of underlying ParlaySocket.
      * @returns {Boolean} Requests the ParlaySocket connection status.
      */
     Public.isConnected = function () {
         return Private.socket.isConnected();
     };
     
+    /**
+     * Requests available protocols for connection from the Broker.
+     * @returns {$q.defer.promise} Resolved with available protocols.
+     */
     Public.requestProtocols = function () {
-        return $q(function (resolve, reject) {
-            Private.socket.sendMessage({'type':'broker', 'request':'get_protocols'}, {}, {'type':'broker', 'response':'get_protocols_response'}, function (response) {
-                resolve(response);
-            });
+        
+        return Public.sendRequest('get_protocols', {}).then(function (response) {
+            return Object.keys(response).map(function (protocol_name) {
+                var protocol = response[protocol_name];
+                
+                protocol.name = protocol_name;
+                
+                return protocol;
+            }, response);
         });
     };
     
-    Public.requestDiscovery = function () {
-        return $q(function (resolve, reject) {
-            Private.socket.sendMessage({'type':'broker', 'request':'get_discovery'}, {}, {'type':'broker', 'response':'get_discovery_response'}, function (response) {
-                resolve(response);
-            });
-        });
+    /**
+     * Opens protocol.
+     * @param {Object} configuration - Configuration object we should configure a new protocol connection with.
+     * @returns {$q.defer.promise} Resolve when response is recieved with result of open request from Broker.
+     */
+    Public.openProtocol = function (configuration) {        
+        return Public.sendRequest('open_protocol', {'protocol_name': configuration.protocol.name, 'params': configuration.protocol.parameters});
     };
     
-    Public.sendMessage = function (topics, contents, response_topics) {
+    /**
+     * Closes protocol.
+     * @param {Object} protocol - Configured open protocol object.
+     * @returns {$q.defer.promise} Resolve when response is recieved with result of close request from Broker.
+     */
+    Public.closeProtocol = function (protocol) {
+        return Public.sendRequest('close_protocol', {'protocol_name': protocol.name, 'params': protocol.parameters});
+    };
+    
+    /**
+     * Request the Broker for a discovery.
+     * @returns {$q.defer.promise} Resolve when response is recieved with available endpoints.
+     */
+    Public.requestDiscovery = function () {        
+        return Public.sendRequest('get_discovery', {});
+    };
+    
+    /**
+     * Sends request message to Broker.
+     * @returns {$q.defer.promise} Resolve when response is recieved.
+     */
+    Public.sendRequest = function (request, contents) {
+        return Private.sendMessage({request: request}, contents, {response: request + '_response'});
+    };
+    
+    /**
+     * Sends message to the Broker.
+     * @returns {$q.defer.promise} Resolve when response is recieved.
+     */
+    Private.sendMessage = function (topics, contents, response_topics) {
+        topics.type = 'broker';
+        response_topics.type = 'broker';
+        
         return $q(function (resolve, reject) {
             Private.socket.sendMessage(topics, contents, response_topics, function (response) {
                 resolve(response);
--- vendor_components/promenade/tests/broker.js
+++ vendor_components/promenade/tests/broker.js
@@ -1,3 +1,97 @@
 (function () {
     "use strict";
+    
+    describe('promenade.broker', function() {
+        
+        beforeEach(module('promenade.broker'));
+        
+        beforeEach(function () {
+            function mockParlaySocket () {
+                
+                var Public = {
+                    connected: false
+                };
+                
+                Public.open = function () {
+                    Public.connected = true;
+                };
+                
+                Public.close = function () {
+                    Public.connected = false;
+                };
+                
+                Public.isConnected = function () {
+                    return Public.connected;
+                };
+                
+                Public.sendMessage = function () {
+                    //
+                };
+                
+                return Public;
+            }
+            
+            module(function ($provide) {
+                $provide.value('ParlaySocket', mockParlaySocket);
+            });
+        });
+        
+        describe('PromenadeBroker', function () {
+            var PromenadeBroker;
+            
+            beforeEach(inject(function(_PromenadeBroker_) {
+                PromenadeBroker = _PromenadeBroker_;
+            }));
+            
+            describe('initialization', function () {
+                
+                it('is not connected', function () {
+                    expect(PromenadeBroker.isConnected()).toBeFalsy(); 
+                });
+                
+            });
+            
+            describe('connection', function () {
+                
+                it('connects', function () {
+                    expect(PromenadeBroker.isConnected()).toBeFalsy();
+                    PromenadeBroker.connect();
+                    expect(PromenadeBroker.isConnected()).toBeTruthy();
+                });
+                
+                it('disconnects', function () {
+                    expect(PromenadeBroker.isConnected()).toBeFalsy();
+                    PromenadeBroker.connect();
+                    expect(PromenadeBroker.isConnected()).toBeTruthy();
+                    PromenadeBroker.disconnect();
+                    expect(PromenadeBroker.isConnected()).toBeFalsy();
+                });
+                
+            });
+            
+            xdescribe('transaction', function () {
+                
+                it('requests discovery', function () {
+                    
+                });
+                
+                it('requests protocols', function () {
+                    
+                });
+                
+                it('opens a protocol', function () {
+                    
+                });
+                
+                it('closes a protocol', function () {
+                    
+                });
+                
+            });
+                
+        });
+
+    });
+    
+    
 }());

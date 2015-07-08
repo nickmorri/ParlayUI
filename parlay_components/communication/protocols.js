d6eb4a825b72cd6bb2830e9a2f0e604ba2d77f2d
Nick Morris <nick@promenadesoftware.com>
2015-07-08 10:17:44 -0700

    feat|doc: Refactored parlay.protocols and documented components

--- parlay_components/communication/protocols.js
+++ parlay_components/communication/protocols.js
@@ -14,7 +14,7 @@ protocols.factory('Protocol', function () {
     };
 });
 
-protocols.factory('ProtocolManager', ['Protocol', 'PromenadeBroker', function (Protocol, PromenadeBroker) {
+protocols.factory('ProtocolManager', ['Protocol', 'PromenadeBroker', '$q', function (Protocol, PromenadeBroker, $q) {
     
     var Private = {
         open: [],
@@ -23,55 +23,112 @@ protocols.factory('ProtocolManager', ['Protocol', 'PromenadeBroker', function (P
     
     var Public = {};
     
+    /**
+     * Public Methods
+     */
+    
+    /**
+     * Returns cached available protocols.
+     * @returns {Array} available protocols.
+     */
     Public.getAvailableProtocols = function () {
         return Private.available;
     };
     
+    /**
+     * Returns cached open protocols.
+     * @returns {Array} open protocols.
+     */
     Public.getOpenProtcols = function () {
         return Private.open;
     };
     
+    /**
+     * Requests the Broker to open a protocol.
+     * @param {Object} configuration - Contains protocol configuration parameters.
+     * @returns {$q.defer.promise} Resolved when the Broker responds with the open result.
+     */
     Public.openProtocol = function (configuration) {
         return PromenadeBroker.openProtocol(configuration);
     };
     
+    /**
+     * Requests the Broker to close a protocol.
+     * @param {Object} protocol - The protocol to be closed
+     * @returns {$q.defer.promise} Resolved when the Broker responds with the close result.
+     */
     Public.closeProtocol = function (protocol) {
         return PromenadeBroker.closeProtocol(protocol);
     };
     
+    /**
+     * Private Methods
+     */
+    
+    /**
+     * Requests both available and open protocols.
+     * @returns {$q.defer.promise} Resolved when both request responses are received.
+     */
     Private.requestProtocols = function () {
-        Private.requestAvailableProtocols();
-        Private.requestOpenProtocols();
+        return $q.all([Private.requestAvailableProtocols(), Private.requestOpenProtocols()]);
     };
     
-    Private.requestOpenProtocols = function () {
-        return PromenadeBroker.requestOpenProtocols();
-    };
-    
+    /**
+     * Requests available protocols.
+     * @returns {$q.defer.promise} Resolved when request response is recieved.
+     */
     Private.requestAvailableProtocols = function () {
         return PromenadeBroker.requestAvailableProtocols();
     };
     
+    /**
+     * Requests open protocols.
+     * @returns {$q.defer.promise} Resolved when request response is recieved.
+     */
+    Private.requestOpenProtocols = function () {
+        return PromenadeBroker.requestOpenProtocols();
+    };    
+    
+    /**
+     * Check if a protocol is opened with the given name.
+     * @returns {Boolean} Returns true if it does, false if it doesn't.
+     */
     Private.hasOpenProtocol = function (name) {
         return Private.getOpenProtocol(name) !== undefined;
     };
     
+    /**
+     * Check if a protocol is available with the given name.
+     * @returns {Boolean} Returns true if it does, false if it doesn't.
+     */
     Private.hasAvailableProtocol = function (name) {
         return Private.getAvailableProtocol(name) !== undefined;
     };
     
+    /**
+     * Return a open protocol with the given name.
+     * @returns {Object} Returns Protocol object.
+     */
     Private.getOpenProtocol = function (name) {
         return Private.open.find(function (protocol) {
             return name === protocol.name;
         });
     };
     
+    /**
+     * Return a available protocol with the given name.
+     * @returns {Object} Returns protocol configuration object.
+     */
     Private.getAvailableProtocol = function (name) {
         return Private.available.find(function (protocol) {
             return name === protocol.name;
         });
     };
     
+    /**
+     * Sets private attribute available to an Array of available protocols.
+     * @param {Object} Map of protocol names : protocol details.
+     */
     Private.setAvailableProtocols = function (protocols) {
         Private.available = Object.keys(protocols).map(function (protocol_name) {
             return {
@@ -84,16 +141,28 @@ protocols.factory('ProtocolManager', ['Protocol', 'PromenadeBroker', function (P
         });
     };
     
+    /**
+     * Sets private attribute open to an Array of open protocols.
+     * @param {Array} Array of open protocols.
+     */
     Private.setOpenProtocols = function (protocols) {
         Private.open = protocols.map(function (protocol) {
             return Protocol(protocol);
         });        
     };
     
+    /**
+     * Clears private attributes open and available.
+     */
     Private.clearProtocols = function () {
         Private.open = [];
         Private.available = [];
     };
+    
+    
+    /**
+     * PromenadeBroker callback registrations.
+     */
     
     PromenadeBroker.onOpen(Private.requestProtocols);
     
@@ -121,10 +190,18 @@ protocols.factory('ProtocolManager', ['Protocol', 'PromenadeBroker', function (P
 protocols.controller('ProtocolConnectionController', ['$scope', '$mdDialog', '$mdToast', 'ProtocolManager', function ($scope, $mdDialog, $mdToast, ProtocolManager) {
     $scope.hide = $mdDialog.hide;
     
+    /**
+     * Returns open protocols from ProtocolManager.
+     * @returns {Array} open protocols
+     */
     $scope.getOpenProtocols = function () {
         return ProtocolManager.getOpenProtcols();
     };
     
+    /**
+     * Check if ProtocolManager has open protocols.
+     * @returns {Boolean} true if open protocols exist, false otherwise.
+     */
     $scope.hasOpenProtocols = function () {
         return ProtocolManager.getOpenProtcols().length !== 0;
     };
@@ -173,17 +250,21 @@ protocols.controller('ProtocolConfigurationController', ['$scope', '$mdDialog', 
     
     $scope.search_text = "";
     $scope.selected_protocol = null;
-        
-    $scope.configuration = {};
     
-    $scope.selectProtocol = function (protocol) {
-        $scope.configuration.protocol = protocol;
-    };
-    
+    /**
+     * Returns a protocols that pass the filterFunction generated by the query string.
+     * @param {String} query - name of potential protocol.
+     * @returns {Array} filtered protocols.
+     */
     $scope.querySearch = function (query) {
         return query ? ProtocolManager.getAvailableProtocols().filter(filterFunction(query)) : ProtocolManager.getAvailableProtocols();
     };
     
+    /**
+     * Builds a filter function with the query string.
+     * @param {String} query - name of protocol by which to build the filterFunction. 
+     * @returns {Function} function which will filter protocols by the query string.
+     */    
     function filterFunction(query) {
         var lowercaseQuery = angular.lowercase(query);
         return function filterFn(protocol) {
@@ -191,12 +272,20 @@ protocols.controller('ProtocolConfigurationController', ['$scope', '$mdDialog', 
         };
     }
     
+    /**
+     * Rejects the $mdDialog promise.
+     * @returns {$q.defer.promise} Rejects $mdDialog promise.
+     */
     $scope.cancel = function () {
         $mdDialog.cancel();
     };
     
+    /**
+     * Resolves the $mdDialog promise with the a configured $scope.selected_protocol.
+     * @returns {$q.defer.promise} Resolves the $mdDialog promise with the a configured $scope.selected_protocol.
+     */
     $scope.accept = function () {
-        $mdDialog.hide($scope.configuration);
+        $mdDialog.hide($scope.selected_protocol);
     };
     
 }]);

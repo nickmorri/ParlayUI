cf34c44dca0e4629b4c4cf99d5d02ea104964a9e
Nick Morris <nick@promenadesoftware.com>
2015-07-10 14:10:00 -0700

    fix: Removing old navigation menu

--- parlay_components/navigation/navigation.js
+++ parlay_components/navigation/navigation.js
@@ -1,74 +1,8 @@
 var navigation = angular.module('parlay.navigation', ['ui.router', 'ngMaterial', 'ngMdIcons', 'promenade.broker', 'parlay.protocols', 'templates-main']);
 
-navigation.value('parlayNavToggleOpen', true);
-
-navigation.controller('parlayToolbarController', ['$scope', '$state', '$mdSidenav', '$mdMedia', 'parlayNavToggleOpen', function ($scope, $state, $mdSidenav, $mdMedia, parlayNavToggleOpen) {
-    
+navigation.controller('parlayToolbarController', ['$scope', '$state', '$mdSidenav', '$mdMedia', function ($scope, $state, $mdSidenav, $mdMedia) {
     // Allows view to access information from $state object. Using $current.self.name for display in toolbar
     $scope.$state = $state;
-    
-    // If we are on a screen size greater than the medium layout breakpoint we should open the navigation menu by default
-    $scope.parlayNavToggleOpen = $mdMedia('gt-md');
-    
-    // Toggle the boolean holding the navigation menu open
-    $scope.toggleMenu = function () {
-        $scope.parlayNavToggleOpen = !$scope.parlayNavToggleOpen;
-    };
-    
-    // If the media query for greater than medium screen size breakpoint is false we should collapse the menu
-    $scope.$watch(function () {
-        return $mdMedia('gt-md');
-    }, function (greater_than) {
-        if (!greater_than && $mdSidenav('parlayNavMenu').isLockedOpen()) $scope.toggleMenu();
-    });    
-    
-}]);
-
-navigation.controller('parlayNavController', ['$scope', '$state', '$rootScope', 'parlayNavToggleOpen', function ($scope, $state, $rootScope, parlayNavToggleOpen) {
-            
-    $scope.parlayNavToggleOpen = parlayNavToggleOpen;
-    
-    // Will be set to true after first successful state change
-    $scope.initialStateChange = false;
-    
-    // Default icons
-    $scope.fetchIcon = function (state) {
-        var icons = {
-            'endpoints': 'extension',
-            'editor': 'create',
-            'console': 'message'
-        };
-        
-        if (icons.hasOwnProperty(state)) return icons[state];
-        else return '';
-    };
-    
-    // Retrieve and format state information to build navigation elements
-    $scope.buildNavFromStates = function () {
-        var excluded = ['settings', 'help'];
-        return $state.get().filter(function (state) {
-            return !state.abstract && excluded.indexOf(state.name) === -1;
-        }).map(function (state) {
-            return {
-                name: state.name,
-                route: state.name,
-                icon: $scope.fetchIcon(state.name)
-            };
-        });
-    };
-    
-    // Retrieve states so we can dynamically build navigation menu
-    $scope.states = $scope.buildNavFromStates();
-    
-    // Close navigation menu after successful state change
-    $rootScope.$on('$stateChangeSuccess', function (event, toState, toParams, fromState, fromParams) {
-        if (!$scope.initialStateChange) {
-            $scope.initialStateChange = true;
-            return;
-        }
-        
-        $scope.parlayNavToggleOpen = false;
-    });
 }]);
 
 navigation.controller('ParlayConnectionListController', ['$scope', '$mdDialog', '$mdToast', 'ProtocolManager', 'PromenadeBroker', function ($scope, $mdDialog, $mdToast, ProtocolManager, PromenadeBroker) {
@@ -195,14 +129,5 @@ navigation.directive('parlayToolbar', function () {
     return {
         templateUrl: '../parlay_components/navigation/directives/parlay-toolbar.html',
         controller: 'parlayToolbarController'
-    };
-});
-
-/* istanbul ignore next */
-navigation.directive('parlayNav', function () {
-    return {
-        templateUrl: '../parlay_components/navigation/directives/parlay-navigation.html',
-        replace: true,
-        controller: 'parlayNavController'
     };
 });
--- parlay_components/navigation/test/navigation.js
+++ parlay_components/navigation/test/navigation.js
@@ -5,42 +5,7 @@
     
         beforeEach(module('parlay.navigation'), module('ngMaterial'));
         
-        describe('parlayNavToggleOpen', function () {
-            
-            it ('defaults open', inject(function ($injector) {
-                expect($injector.get('parlayNavToggleOpen')).toBeTruthy();
-            }));
-        });
-        
-    	describe('parlayToolbarController', function () {
-    		var scope, parlayToolbarController, mediaSize;
-    
-    		beforeEach(inject(function($rootScope, $controller) {
-        		mediaSize = 'md';
-    			scope = $rootScope.$new();
-    			parlayToolbarController = $controller('parlayToolbarController', {$scope: scope, ParlaySocket: function () {
-        			return {
-            			close: function () {
-                			// Do something
-            			},
-            			open: function () {
-                			// Do something
-            			}
-        			};
-    			}});
-    		}));
-    		
-    		describe('toggle nav menu', function () {
-        		it('toggles', inject(function ($mdMedia) {
-            		expect(scope.parlayNavToggleOpen).toBe($mdMedia('gt-md'));
-            		scope.toggleMenu();
-            		expect(scope.parlayNavToggleOpen).toBe(!$mdMedia('gt-md'));
-        		}));
-    		});
-    
-    	});
-    	
-    	describe('parlayConnectionStatusController', function () {
+        describe('parlayConnectionStatusController', function () {
         	var scope, parlayConnectionStatusController, MockBroker;
         	
         	beforeEach(inject(function($rootScope, $controller) {
@@ -106,44 +71,6 @@
         	});
         	
         });
-    	
-    	describe('parlayNavController', function () {
-    		var scope, parlayNavController;
-    		
-    		beforeEach(inject(function($rootScope, $controller) {
-    			scope = $rootScope.$new();
-    			parlayNavController = $controller('parlayNavController', {$scope: scope, $state: {
-            		get: function () {
-            		    return [{name: 'editor'}, {name:'settings'}];
-        		    }
-                }});
-    		}));
-    		
-    		describe('fetches icon for', function () {
-        		
-        		it('endpoints', function () {
-            		expect(scope.fetchIcon('endpoints')).toBe('extension');
-        		});
-        		
-        		it('editor', function () {
-            		expect(scope.fetchIcon('editor')).toBe('create');
-        		});
-        		
-        		it('console', function () {
-            		expect(scope.fetchIcon('console')).toBe('message');
-        		});
-        		
-        		it('non existant state', function () {
-            		expect(scope.fetchIcon('NULL')).toBe('');
-        		});
-        		
-    		});
-    		
-    		it('builds nav menu from states', function () {
-        		expect(scope.states).toEqual([{name:"editor", route:"editor", icon:"create"}]);
-    		});
-    
-    	});
         
     });
 }());

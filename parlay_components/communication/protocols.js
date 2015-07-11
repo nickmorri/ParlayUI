921ef8d4ff05cdf6ecdb94e80dfbcb876dcbc2d5
Nick Morris <nick@promenadesoftware.com>
2015-07-10 17:51:24 -0700

    feat: Moved connections from navigation

--- parlay_components/communication/protocols.js
+++ parlay_components/communication/protocols.js
@@ -79,7 +79,10 @@ protocols.factory('ProtocolManager', ['Protocol', 'PromenadeBroker', '$q', funct
      * @returns {$q.defer.promise} Resolved when the Broker responds with the close result.
      */
     Public.closeProtocol = function (protocol) {
-        return PromenadeBroker.closeProtocol(protocol);
+        return PromenadeBroker.closeProtocol(protocol).then(function (response) {
+            if (response.status !== 'ok') return $q.reject(response);
+            else return response;
+        });
     };
     
     /**
@@ -261,4 +264,118 @@ protocols.controller('ProtocolConfigurationController', ['$scope', '$mdDialog', 
         $mdDialog.hide($scope.selected_protocol);
     };
     
+}]);
+
+protocols.controller('ParlayConnectionListController', ['$scope', '$mdDialog', '$mdToast', 'ProtocolManager', 'PromenadeBroker', function ($scope, $mdDialog, $mdToast, ProtocolManager, PromenadeBroker) {
+    
+    $scope.hide = $mdDialog.hide;
+    $scope.connection_icon = 'cloud_off';
+    
+    /**
+     * Returns Broker connection status.
+     * @returns {Boolean} Broker connection status
+     */
+    $scope.isBrokerConnected = function () {
+        return PromenadeBroker.isConnected();
+    };
+    
+    /**
+     * Returns Broker location.
+     * @returns {String} location of Broker where WebSocket is connected to.
+     */
+    $scope.getBrokerAddress = function () {
+        return PromenadeBroker.getBrokerAddress();  
+    };
+    
+    /**
+     * Switches Broker connected and disconnected.
+     */
+    $scope.toggleBrokerConnection = function () {
+        if (PromenadeBroker.isConnected()) PromenadeBroker.disconnect();
+        else PromenadeBroker.connect();
+    };
+    
+    /**
+     * Returns open protocols from ProtocolManager.
+     * @returns {Array} open protocols
+     */
+    $scope.getOpenProtocols = function () {
+        return ProtocolManager.getOpenProtocols();
+    };
+    
+    /**
+     * Check if ProtocolManager has open protocols.
+     * @returns {Boolean} true if open protocols exist, false otherwise.
+     */
+    $scope.hasOpenProtocols = function () {
+        return ProtocolManager.getOpenProtocols().length !== 0;
+    };
+    
+    /**
+     * Closes protocol then spawns toast notifying user.
+     * @param {Object} protocol - Protocol configuration object.
+     */
+    $scope.closeProtocol = function (protocol) {
+        ProtocolManager.closeProtocol(protocol).then(function (result) {
+            $mdToast.show($mdToast.simple()
+                .content('Closed ' + protocol.name + '.'));
+        }).catch(function (result) {
+            $mdToast.show($mdToast.simple()
+                .content(result.status));  
+        });
+    };
+    
+    /**
+     * Show protocol configuration dialog and have ProtocolManager open a protocol.
+     * @param {Event} - Event generated when button is selected. Allows use to have origin for dialog display animation.
+     */
+    $scope.openConfiguration = function (event) {
+        $mdDialog.hide();
+        // Show a configuraton dialog allowing us to setup a protocol configuration.
+        $mdDialog.show({
+            targetEvent: event,
+            controller: 'ProtocolConfigurationController',
+            templateUrl: '../parlay_components/communication/directives/parlay-protocol-configuration-dialog.html',
+        }).then(function (configuration) {
+            return ProtocolManager.openProtocol(configuration);
+        }).catch(function (result) {
+            return undefined;
+        }).then(function (response) {
+            if (response === undefined) return response;
+            // Don't display anything if we didn't open a protocol.
+            $mdToast.show($mdToast.simple()
+                .content('Connected successfully to protocol.')
+                .position('bottom left').hideDelay(3000));
+        }, function (response) {
+            $mdToast.show($mdToast.simple()
+                .content('Failed to make protocol connection.')
+                .position('bottom left').hideDelay(3000));
+        });        
+    };
+    
+    $scope.$watch(function () {
+        return PromenadeBroker.isConnected();
+    }, function (connected) {
+        $scope.connection_icon = connected ? 'cloud' : 'cloud_off';
+    });
+    
+}]);
+
+protocols.controller('ParlayConnectionStatusController', ['$scope', '$mdDialog', 'PromenadeBroker', function ($scope, $mdDialog, PromenadeBroker) {
+    $scope.connection_icon = 'cloud_off';
+        
+    $scope.viewConnections = function (event) {
+        $mdDialog.show({
+            targetEvent: event,
+            clickOutsideToClose: true,
+            controller: 'ParlayConnectionListController',
+            templateUrl: '../parlay_components/communication/directives/parlay-connection-list-dialog.html'
+        });
+    };
+    
+    $scope.$watch(function () {
+        return PromenadeBroker.isConnected();
+    }, function (connected) {
+        $scope.connection_icon = connected ? 'cloud' : 'cloud_off';
+    });
 }]);

5e62836213ae27829e23df32bb9cd29862d2dbf0
Nick Morris <nick@promenadesoftware.com>
2015-07-27 14:51:50 -0700

    feat: Changed theme to green

--- app.js
+++ app.js
@@ -1,9 +1,9 @@
 var parlay = angular.module('parlay.main', ['ui.router', 'parlay.socket', 'parlay.navigation', 'parlay.endpoints']);
 
 parlay.config(function ($stateProvider, $urlRouterProvider, $mdThemingProvider) {
-    
+        
     // Theme configuration
-    $mdThemingProvider.theme('default').accentPalette('red');
+    $mdThemingProvider.theme('default').primaryPalette('green').accentPalette('red');
     
     // Default state for unmatched URLs
     $urlRouterProvider.otherwise('/endpoints');

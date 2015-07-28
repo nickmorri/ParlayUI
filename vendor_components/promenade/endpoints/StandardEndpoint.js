b59bb7e8b925bb2bb846f52611d2366188d38a0b
Nick Morris <nick@promenadesoftware.com>
2015-07-27 17:17:01 -0700

    feat: Required field validation

--- vendor_components/promenade/endpoints/StandardEndpoint.js
+++ vendor_components/promenade/endpoints/StandardEndpoint.js
@@ -221,7 +221,8 @@ standard_endpoint.directive('promenadeStandardEndpointCardCommandContainer', ['R
     return {
         scope: {
             message: "=",
-            fields: "="
+            fields: "=",
+            commandForm: "="
         },
         templateUrl: '../vendor_components/promenade/endpoints/directives/promenade-standard-endpoint-card-command-container.html',
         compile: function (element) {

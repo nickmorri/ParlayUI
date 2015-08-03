d1dc608cff8c5f84e8e34ebd23cd6ff6989047c0
Nick Morris <nickmorri@gmail.com>
2015-08-03 13:53:03 -0700

    feat: Added StandardEndpointLog filtering

--- vendor_components/promenade/endpoints/StandardEndpointLogController.js
+++ vendor_components/promenade/endpoints/StandardEndpointLogController.js
@@ -2,6 +2,17 @@ var standard_endpoint_log = angular.module('promenade.endpoints.standardendpoint
 
 standard_endpoint_log.controller('PromenadeStandardEndpointCardLogController', ['$scope', function ($scope) {
     
+    $scope.getFilteredLog = function (query) {
+	    var lower_case_query = angular.lowercase(query);
+	    return query ? $scope.endpoint.log.filter(function (message) {
+		    return Object.keys(message.TOPICS).some(function (key) {
+			    return angular.lowercase(message.TOPICS[key].toString()).indexOf(lower_case_query) > -1;
+		    }) || Object.keys(message.CONTENTS).some(function (key) {
+			    return angular.lowercase(message.CONTENTS[key].toString()).indexOf(lower_case_query) > -1;
+		    });
+	    }) : $scope.endpoint.log;
+    };
+    
     $scope.getLog = function () {
         return $scope.endpoint.log;
     };

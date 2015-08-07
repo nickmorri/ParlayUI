37d15093f0de4cdff9f2aaa436d90884563a493d
Nick Morris <nick@promenadesoftware.com>
2015-08-07 16:20:51 -0700

    fix: Active tab is persisted

--- parlay_components/endpoints/ParlayEndpoint.js
+++ parlay_components/endpoints/ParlayEndpoint.js
@@ -60,9 +60,59 @@ parlay_endpoint.factory('ParlayEndpoint', function () {
 parlay_endpoint.directive('parlayEndpointCard', ['$compile', 'ParlayLocalStore', function ($compile, ParlayLocalStore) {
     return {
         templateUrl: '../parlay_components/endpoints/directives/parlay-endpoint-card.html',
-        controller: ['$scope', 'ParlayLocalStore', function ($scope, ParlayLocalStore) {
-	        
-	        function setAttr(directive) {
+        link: function (scope, element, attributes) {
+            
+            function compileDirectives() {
+	        	scope.endpoint = scope.container.ref;
+            
+	            // Converts directive names to snake-case which Angular requires during directive compilation.
+	            function snake_case(name) {
+	                return name.replace(/[A-Z]/g, function(letter, pos) {
+	                    return (pos ? '-' : '') + letter.toLowerCase();
+	                });
+	            }
+	            
+	            // Locate locations where we are going to insert dynamic directives.
+	            var toolbar = element[0].querySelector('div.md-toolbar-tools');
+	            var tabs = element[0].querySelector('md-tabs');
+	            
+	            var endpoints = scope.endpoint.getDirectives();
+	            
+	            // Append toolbar directives.
+	            endpoints.filter(function (endpoint) {
+	                return endpoint.hasOwnProperty('toolbar');
+	            }).reduce(function (previous, endpoint) {
+	                return previous.concat(endpoint.toolbar.map(function (directive) {
+	                    return '<' + snake_case(directive, '-') + ' endpoint="endpoint" layout-fill layout="row" layout-align="space-between center"></' + snake_case(directive, '-') + '>';    
+	                }));
+	            }, []).forEach(function (directive_string) {
+		            toolbar.insertBefore($compile(directive_string)(scope)[0], toolbar.firstChild);
+	            });
+	            
+	            // Append tabs directives.
+	            endpoints.filter(function (endpoint) {
+	                return endpoint.hasOwnProperty('tabs');
+	            }).reduce(function (previous, endpoint) {
+	                return previous.concat(endpoint.tabs.map(function (directive) {
+	                    return '<' + snake_case(directive, '-') + ' endpoint="endpoint"></' + snake_case(directive, '-') + '>';
+	                }));
+	            }, []).forEach(function (directive_string) {
+	                tabs.appendChild($compile(directive_string)(scope)[0]);
+	            });
+            }
+            
+            function setActiveTab(newValue, oldValue) {
+	            try {
+		        	var saved_state = ParlayLocalStore.getDirectiveContainer('parlayEndpointCard.' + scope.endpoint.name.replace(' ', '_') + '_' + scope.container.uid);
+		            if (saved_state) scope.active_tab_index = saved_state.active_tab_index;
+	            } catch(error) {
+		            throw error;
+		            // Maybe we should do something about a cache miss but for now during DEVELOPMENT do nothing.
+	            }
+	            scope.$watch('active_tab_index', setAttr(key));
+            }
+            
+            function setAttr(directive) {
 		        return function () {
 			    	ParlayLocalStore.set(directive.replace(' ', '_'), this.exp, this.last);    
 		        };		        
@@ -74,57 +124,12 @@ parlay_endpoint.directive('parlayEndpointCard', ['$compile', 'ParlayLocalStore',
 				};
 			}
 	        
-	        var key = 'parlayEndpointCard.' + $scope.container.ref.name + '_' + $scope.container.uid;
+	        var key = 'parlayEndpointCard.' + scope.container.ref.name.replace(' ', '_') + '_' + scope.container.uid;
 	        
-	        $scope.$watch('active_tab_index', setAttr(key));
-	        $scope.$watch('$index', setAttr(key));
-	        $scope.$on('$destroy', removeItem(key));
-        }],
-        link: function (scope, element, attributes) {
+	        compileDirectives();
+            setActiveTab();
             
-            scope.endpoint = scope.container.ref;
-            
-            // Converts directive names to snake-case which Angular requires during directive compilation.
-            function snake_case(name) {
-                return name.replace(/[A-Z]/g, function(letter, pos) {
-                    return (pos ? '-' : '') + letter.toLowerCase();
-                });
-            }
-            
-            // Locate locations where we are going to insert dynamic directives.
-            var toolbar = element[0].querySelector('div.md-toolbar-tools');
-            var tabs = element[0].querySelector('md-tabs');
-            
-            var endpoints = scope.endpoint.getDirectives();
-            
-            // Append toolbar directives.
-            endpoints.filter(function (endpoint) {
-                return endpoint.hasOwnProperty('toolbar');
-            }).reduce(function (previous, endpoint) {
-                return previous.concat(endpoint.toolbar.map(function (directive) {
-                    return '<' + snake_case(directive, '-') + ' endpoint="endpoint" layout-fill layout="row" layout-align="space-between center"></' + snake_case(directive, '-') + '>';    
-                }));
-            }, []).forEach(function (directive_string) {
-	            toolbar.insertBefore($compile(directive_string)(scope)[0], toolbar.firstChild);
-            });
-            
-            // Append tabs directives.
-            endpoints.filter(function (endpoint) {
-                return endpoint.hasOwnProperty('tabs');
-            }).reduce(function (previous, endpoint) {
-                return previous.concat(endpoint.tabs.map(function (directive) {
-                    return '<' + snake_case(directive, '-') + ' endpoint="endpoint"></' + snake_case(directive, '-') + '>';
-                }));
-            }, []).forEach(function (directive_string) {
-                tabs.appendChild($compile(directive_string)(scope)[0]);
-            });
-            
-            try {
-	        	var saved_state = ParlayLocalStore.getDirectiveContainer('parlayEndpointCard.' + scope.endpoint.name.replace(' ', '_') + '_' + scope.container.uid);
-	            if (saved_state) scope.active_tab_index = container.active_tab_index;
-            } catch(error) {
-	            // Maybe we should do something about a cache miss but for now during DEVELOPMENT do nothing.
-            }
+	        scope.$on('$destroy', removeItem(key));
             
         }
     };

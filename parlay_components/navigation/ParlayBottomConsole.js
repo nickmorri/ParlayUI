7c21067e1ccf605b9458d49edba4116072636d54
Nick Morris <nick@promenadesoftware.com>
2015-08-27 14:04:14 -0700

    refactor: Cleaning up a little bit

--- parlay_components/navigation/ParlayBottomConsole.js
+++ parlay_components/navigation/ParlayBottomConsole.js
@@ -11,29 +11,47 @@ bottom_bar.factory('ScriptLogger',  function () {
     };
 });
 
-/* istanbul ignore next */
 bottom_bar.controller('ParlayConsoleBarController', ['$scope', '$timeout', 'PromenadeBroker', 'ScriptLogger', function ($scope, $timeout, PromenadeBroker, ScriptLogger) {
 
-    $scope.console_hidden = true;
-    $scope.console_log = [];
+    var console_hidden = true;
+    var console_log = [];
+    var console_filtering = false;
+    
     $scope.console_command = undefined;
     $scope.console_icon = 'unfold_more';
     $scope.received_message_status_icon = 'radio_button_off';
-    $scope.filtering = false;
+
     $scope.console_filter = undefined;
     
-    $scope.consoleToggleConsole = function() {
-		$scope.console_hidden = !$scope.console_hidden;
+    $scope.toggleConsole = function() {
+		console_hidden = !console_hidden;
+		$scope.console_icon = console_hidden ? 'unfold_more' : 'unfold_less';
+    };
+    
+    $scope.isConsoleHidden = function () {
+	    return console_hidden;
     };
     
     // Call this to add strings to the log
     $scope.consoleAddToLog = function (statement, user) {
-	    $scope.console_log.push({
+	    console_log.push({
 		    timeStamp: Date.now(),
 		    statement: statement,
 		    user: user
 		});
 	};
+	
+	$scope.getConsoleLog = function () {
+	    return console_log;
+    };
+    
+    $scope.hasConsoleLog = function () {
+	    return $scope.getConsoleLog().length > 0;
+    };
+
+	$scope.clearConsoleLog = function (event) {
+		console_log = [];
+    };
 
     $scope.consoleSendCommand = function() {
         $scope.consoleAddToLog($scope.console_command, true);
@@ -48,32 +66,18 @@ bottom_bar.controller('ParlayConsoleBarController', ['$scope', '$timeout', 'Prom
         $scope.console_command = undefined;
     };
     
-    $scope.getConsoleLog = function () {
-	    return $scope.console_log;
-    };
     
-    $scope.hasConsoleLog = function () {
-	    return $scope.getConsoleLog().length > 0;
-    };
-    
-    $scope.clearConsoleLog = function (event) {
-	    $scope.console_log = [];
-    };
     
     $scope.toggleConsoleLogFilter = function () {
-	    $scope.filtering = !$scope.filtering;
-	    if (!$scope.filtering) $scope.console_filter = undefined;
+	    console_filtering = !console_filtering;
+	    if (!console_filtering) $scope.console_filter = undefined;
     };
     
-    $scope.hasFilter = function () {
-	    return $scope.filtering;
+    $scope.hasConsoleLogFilter = function () {
+	    return console_filtering;
     };
-    
-    $scope.$watch('console_hidden', function (newValue) {
-	    $scope.console_icon = newValue ? 'unfold_more' : 'unfold_less';
-    });
 
-    //set up the script logger for other controllers to use
+    // Set up the ScriptLogger for other controllers to use.
     ScriptLogger.logCommand = $scope.consoleAddToLog;
     ScriptLogger.logResponse = $scope.consoleAddresponseToLog;
 }]);

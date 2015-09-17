f9aea336cbff35b6de91c4f3ad5eb016d4d73890
Nick Morris <nick@promenadesoftware.com>
2015-09-17 11:05:30 -0700

    doc(DirectMessage): Added comments

--- vendor_components/promenade/protocols/DirectMessage.js
+++ vendor_components/promenade/protocols/DirectMessage.js
@@ -3,7 +3,7 @@ var direct_message = angular.module('promenade.protocols.directmessage', ['parla
 direct_message.factory('PromenadeDirectMessageProtocol', ['ParlayProtocol', 'PromenadeStandardEndpoint', function (ParlayProtocol, PromenadeStandardEndpoint) {
     
     function PromenadeDirectMessageProtocol(configuration) {
-        'use strict';
+        "use strict";
         // Call the constructor of the prototype we inherit from.
         ParlayProtocol.call(this, configuration);
         
@@ -17,14 +17,27 @@ direct_message.factory('PromenadeDirectMessageProtocol', ['ParlayProtocol', 'Pro
     // Create a Object that inherits from the prototype of ParlayProtocol.
     PromenadeDirectMessageProtocol.prototype = Object.create(ParlayProtocol.prototype);
     
+    /**
+	 * Consumes a message ID by incrementing then returning this new ID.
+	 * @returns {Number} - current message ID
+	 */
     PromenadeDirectMessageProtocol.prototype.consumeMessageId = function () {
         return ++this.current_message_id;
     };
     
+    /**
+	 * Returns the current message ID.
+	 * @returns {Number} - current message ID.
+	 */
     PromenadeDirectMessageProtocol.prototype.getMessageId = function () {
         return this.current_message_id;
     };
     
+    /**
+	 * Constructs message topics from the partially built given message.
+	 * @param {Object} message - partially constructed message.
+	 * @returns {Object} - partially constructed message with topics.
+	 */
     PromenadeDirectMessageProtocol.prototype.buildMessageTopics = function (message) {
         var new_message = angular.copy(message);
         new_message.TOPICS.MSG_ID = this.consumeMessageId();
@@ -32,6 +45,11 @@ direct_message.factory('PromenadeDirectMessageProtocol', ['ParlayProtocol', 'Pro
         return new_message;
     };
     
+    /**
+	 * Constructs response topics from the partially built given message.
+	 * @param {Object} message - partially constructed message, includes topics.
+	 * @returns {Object} - partially construtcted message with response topics.
+	 */
     PromenadeDirectMessageProtocol.prototype.buildResponseTopics = function (message) {
         return {
             FROM: message.TOPICS.TO,
@@ -40,6 +58,11 @@ direct_message.factory('PromenadeDirectMessageProtocol', ['ParlayProtocol', 'Pro
         };
     };
     
+    /**
+	 * Sends a constructed message over our protocol.
+	 * @param {Object} message - partially constructed message, we add topics and response topics.
+	 * @returns {$q.defer.Promise} - resolved when we receive a response.
+	 */
     PromenadeDirectMessageProtocol.prototype.sendCommand = function (message) {
         var new_message = this.buildMessageTopics(message);
         var response_topics = this.buildResponseTopics(new_message);

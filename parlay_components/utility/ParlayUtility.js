73632466f88fe74d3f5e36ef9b17a7ed41fbe9cc
Nick Morris <nick@promenadesoftware.com>
2015-10-22 10:45:09 -0700

    refactor(ParlayUtility): Added docstrings and made JS class

--- parlay_components/utility/ParlayUtility.js
+++ parlay_components/utility/ParlayUtility.js
@@ -1,17 +1,40 @@
-function ParlayUtility() {
-	return {
-		// Converts directive names to snake-case which Angular requires during directive compilation.
-        snakeCase: function(name) {
-            return name.replace(/[A-Z]/g, function(letter, pos) {
-                return (pos ? '-' : '') + letter.toLowerCase();
-            });
-        },
-        // Traverses up scope tree looking for the target scope attribute
-        relevantScope: function(currentScope, attribute) {
-		    return currentScope.hasOwnProperty(attribute) ? currentScope : currentScope.hasOwnProperty('$parent') && currentScope.$parent !== null ? this.relevantScope(currentScope.$parent, attribute) : undefined;
-		}
-	};
+/**
+ * Convenience class with useful methods that can be injected throughout Parlay.
+ * @constructor
+ */
+function ParlayUtility() {}
+
+/**
+ * Converts directive names to snake-case which Angular requires during directive compilation.
+ * @param {String} name - Any cased String.
+ * @returns {String} - snake-cased String.
+ */
+ParlayUtility.prototype.snakeCase = function(name) {
+    return name.replace(/[A-Z]/g, function(letter, position) {
+        return (position ? '-' : '') + letter.toLowerCase();
+    });
+};
+
+/**
+ * Traverses given scope object until scope is found where attribute exists.
+ * @param {AngularJS $scope} scope - AngularJS $scope object to begin searching for the attribute on.
+ * @param {String} attribute - Name of the attribute we are searching for.
+ */
+ParlayUtility.prototype.relevantScope = function(scope, attribute) {
+	// Return scope if it has the attribute we are looking for.
+	if (scope.hasOwnProperty(attribute))
+		return scope;
+	// If the scope has a parent scope that isn't null continue searching up the scope tree.
+	else if (scope.hasOwnProperty('$parent') && scope.$parent !== null)
+		return this.relevantScope(scope.$parent, attribute);
+	// Otherwise the attribute doesn't exist on the scope tree and we should return undefined.
+	else
+		return undefined;
+};
+
+function ParlayUtilityFactory() {
+	return new ParlayUtility();
 }
 
 angular.module('parlay.utility', [])
-	.factory('ParlayUtility', ParlayUtility);
+	.factory('ParlayUtility', ParlayUtilityFactory);

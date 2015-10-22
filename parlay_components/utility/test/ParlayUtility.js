aff9397911c1849ccb3f7c6fd53acba3687d0c92
Nick Morris <nick@promenadesoftware.com>
2015-10-22 11:13:27 -0700

    test(ParlayUtility): relevantScope tests added

--- parlay_components/utility/test/ParlayUtility.js
+++ parlay_components/utility/test/ParlayUtility.js
@@ -1,26 +1,39 @@
 (function () {
-    'use strict';
+    "use strict";
 
-    describe('parlay.utility', function() {
+    describe("parlay.utility", function() {
+        var scope, ParlayUtility;
         
-        beforeEach(module('parlay.utility'));
+        beforeEach(module("parlay.utility"));
+            
+        beforeEach(inject(function (_$rootScope_, _ParlayUtility_) {
+            ParlayUtility = _ParlayUtility_;
+            scope = _$rootScope_.$new();
+        }));
         
-        describe('ParlayEndpointManager', function () {
-            var scope, ParlayUtility;
-            
-            beforeEach(inject(function (_$rootScope_, _ParlayUtility_) {
-                ParlayUtility = _ParlayUtility_;
-                scope = _$rootScope_.$new();
-            }));
-            
-            it("converts strings to snake-case", function() {
+        describe("ParlayUtility", function () {
+	        
+	        it("converts strings to snake-case", function() {
 	            expect(ParlayUtility.snakeCase("helloWorld")).toBe("hello-world");
 	            expect(ParlayUtility.snakeCase("parlayProjectArbitraryLengthStringToBeTested")).toBe("parlay-project-arbitrary-length-string-to-be-tested");
-            });
-            
-            describe("locates attributes on scope", function() {});
-            
+	        });
+	        
+	        it("locates attributes on scope", function() {
+		        scope.test = true;		        
+		        var sub_scope = scope.$new();		        
+		        sub_scope.hey = false;
+		        
+		        expect(ParlayUtility.relevantScope(scope, "test")).toBeDefined();
+		        expect(ParlayUtility.relevantScope(sub_scope, "test")).toBeDefined();
+		        expect(ParlayUtility.relevantScope(sub_scope, "hey")).toBeDefined();
+		        
+		        expect(ParlayUtility.relevantScope(scope, "hey")).toBeUndefined();
+		        expect(ParlayUtility.relevantScope(sub_scope, "woah")).toBeUndefined();
+		        expect(ParlayUtility.relevantScope(scope, "woah")).toBeUndefined();
+	        });
+	        
         });
         
     });
+    
 }());

aa162562914ecd18da2fcc062caa50180b14b2c3
Nick Morris <nick@promenadesoftware.com>
2015-10-26 13:19:40 -0700

    refactor(StandardEndpoint.js): Adding comments

--- vendor_components/promenade/endpoints/StandardEndpoint.js
+++ vendor_components/promenade/endpoints/StandardEndpoint.js
@@ -1,3 +1,8 @@
+/**
+ * Formats and parses a given field. Recursively parses dropdown options fields.
+ * @param {Object} field - Field object to be parsed.
+ * @returns {Object} - Parsed and formatted field Object.
+ */
 function parseField(field) {
 	            
     var fieldObject = {
@@ -31,6 +36,11 @@ function parseField(field) {
 
 function PromenadeStandardEndpointFactory(ParlayEndpoint) {
     
+    /**
+	 * PromenadeStandardEndpoint constructor.
+	 * Prototypically inherits from ParlayEndpoint.
+	 * @constructor
+	 */
     function PromenadeStandardEndpoint(data, protocol) {
 	    // Call our parent constructor first.
         ParlayEndpoint.call(this, data, protocol);
@@ -44,6 +54,7 @@ function PromenadeStandardEndpointFactory(ParlayEndpoint) {
             enumerable: true
         });
         
+        // Adds all available PromenadeEndpointCard tabs.
         this.addAvailableDirective({
 	        tabs: [
 	        	"promenadeStandardEndpointCardCommands",
@@ -53,13 +64,14 @@ function PromenadeStandardEndpointFactory(ParlayEndpoint) {
 	        ]
         });
         
+        // Adds default PromenadeEndpointCard tabs.
         this.addDefaultDirective({
 	        toolbar: ["promenadeStandardEndpointCardToolbar"],
 	        tabs: ["promenadeStandardEndpointCardCommands"]
 	    });
-          
+	    
         if (data.CONTENT_FIELDS) {        
-            Object.defineProperty(this, 'content_fields', {
+            Object.defineProperty(this, "content_fields", {
                 value: data.CONTENT_FIELDS.reduce(function (accumulator, field) {
                     var parsed_field = parseField(field);
                     accumulator[parsed_field.label] = parsed_field;
@@ -114,31 +126,59 @@ function PromenadeStandardEndpointFactory(ParlayEndpoint) {
        } 
     });
     
+    /**
+	 * Checks if query equals id.
+	 * @param {String} query - User inputted query.
+	 * @returns {Boolean} - True if match, false otherwise.
+	 */
     PromenadeStandardEndpoint.prototype.matchesId = function (query) {
         return this.id === query;
     };
     
+    /**
+	 * Checks if query matches type.
+	 * @param {String} query - User inputted query.
+	 * @returns {Boolean} - True if partial or full match, false otherwise.
+	 */
     PromenadeStandardEndpoint.prototype.matchesType = function (query) {
         return angular.lowercase(this.getType()).indexOf(query) > -1;
     };
     
+    /**
+	 * Checks if query matches name.
+	 * @param {String} query - User inputted query.
+	 * @returns {Boolean} - True if partial or full match, false otherwise.
+	 */
     PromenadeStandardEndpoint.prototype.matchesName = function (query) {
         return angular.lowercase(this.name).indexOf(query) > -1;
     };
     
+    /**
+	 * Checks if endpoint has properties that match query.
+	 * @param {String} query - User inputted query.
+	 * @returns {Boolean} - True if partial or full match, false otherwise.
+	 */
     PromenadeStandardEndpoint.prototype.matchesQuery = function (query) {
         var lowercase_query = angular.lowercase(query);
         return this.matchesName(lowercase_query) || this.matchesType(lowercase_query) || this.matchesId(lowercase_query);
     };
     
+    /**
+	 * Returns message ID from the endpoint's protocol.
+	 * @returns {Number} - current message ID.
+	 */
     PromenadeStandardEndpoint.prototype.getMessageId = function () {
         return this.protocol.getMessageId();
     };
     
+    /**
+	 * Generates topics object for the endpoint.
+	 * @returns {Object} - Map of key/value pairs.
+	 */
     PromenadeStandardEndpoint.prototype.generateTopics = function () {
         return {
             TO: this.id,
-            MSG_TYPE: 'COMMAND'
+            MSG_TYPE: "COMMAND"
         };
     };
     
@@ -151,14 +191,36 @@ function PromenadeStandardEndpointFactory(ParlayEndpoint) {
     
     PromenadeStandardEndpoint.prototype.sendMessage = function (message) {
         return this.protocol.sendMessage(this.generateTopics(), this.generateContents(message));
+    /**
+	 * Sends message using the endpoint's protocol.
+	 * @param {Object} contents - Map of key/value pairs.
+	 * @returns {$q.defer.Promise} - Resolved when message response received.
+	 */
+    PromenadeStandardEndpoint.prototype.sendMessage = function (contents) {
+        return this.protocol.sendMessage(this.generateTopics(), contents);
     };
     
+    /**
+	 * Returns function that updates the given stream with the value from the response.
+	 * @param {Object} stream - Data stream object.
+	 * @returns {Function} - Function described below.
+	 */
     PromenadeStandardEndpoint.prototype.buildStreamUpdater = function (stream) {
+	    /**
+		 * Updates the stream object in the closure scope with value from response.
+		 * @param {Object} response - stream response that contains value.
+		 */
 	    return function streamUpdater(response) {
 		    this.data_streams[stream.NAME].value = response.VALUE;
 	    }.bind(this);
     };
     
+    /**
+	 * Sends message requesting a data stream. 
+	 * Then sets up a onMessage listener to update the data stream object.
+	 * @param {Object} stream - Data stream object.
+	 * @returns {$q.defer.Promise} - Resolved when we receive stream response.
+	 */
     PromenadeStandardEndpoint.prototype.requestStream = function (stream) {
 		if (stream.rate < 1) stream.rate = 1;
 	    return this.protocol.sendMessage({
@@ -190,6 +252,12 @@ function PromenadeStandardEndpointFactory(ParlayEndpoint) {
 	    }.bind(this));
     };
     
+    /**
+	 * Sends message canceling a data stream.
+	 * Then removes the onMessage listener that may have been setup during stream request.
+	 * @param {Object} stream - Data stream object.
+	 * @returns {$q.defer.Promise} - Resolved when we receive stream cancelation response.
+	 */
     PromenadeStandardEndpoint.prototype.cancelStream = function (stream) {
 	    stream.rate = 0;
 	    return this.protocol.sendMessage({
@@ -217,6 +285,12 @@ function PromenadeStandardEndpointFactory(ParlayEndpoint) {
 	    }.bind(this));
     };
     
+    /**
+	 * Sends message requesting to get a property.
+	 * Then updates the value of the property stored on the endpoint.
+	 * @param {Object} property - Property object.
+	 * @returns {$q.defer.Promise} - Resolved when we receive a response with property value.
+	 */
     PromenadeStandardEndpoint.prototype.getProperty = function (property) {
 	    return this.protocol.sendMessage({
 		    TX_TYPE: "DIRECT",
@@ -239,6 +313,11 @@ function PromenadeStandardEndpointFactory(ParlayEndpoint) {
 		}.bind(this));
     };
     
+    /**
+	 * Sends message requesting to set a property.
+	 * @param {Object} property - Property object.
+	 * @returns {$q.defer.Promise} - Resolved when we receive a response with confirmation of property set.
+	 */
     PromenadeStandardEndpoint.prototype.setProperty = function (property) {
 	    return this.protocol.sendMessage({
 		    TX_TYPE: "DIRECT",
@@ -269,10 +348,10 @@ function PromenadeStandardEndpointCardToolbar() {
         scope: {
             endpoint: "="
         },
-        templateUrl: '../vendor_components/promenade/endpoints/directives/promenade-standard-endpoint-card-toolbar.html'
+        templateUrl: "../vendor_components/promenade/endpoints/directives/promenade-standard-endpoint-card-toolbar.html"
     };
 }
 
-angular.module('promenade.endpoints.standardendpoint', ['parlay.endpoints', 'promenade.endpoints.standardendpoint.commands', 'promenade.endpoints.standardendpoint.log', 'promenade.endpoints.standardendpoint.graph', 'promenade.endpoints.standardendpoint.property', 'ngOrderObjectBy'])
-	.factory('PromenadeStandardEndpoint', ['ParlayEndpoint', PromenadeStandardEndpointFactory])
-	.directive('promenadeStandardEndpointCardToolbar', PromenadeStandardEndpointCardToolbar);
+angular.module("promenade.endpoints.standardendpoint", ["parlay.endpoints", "promenade.endpoints.standardendpoint.commands", "promenade.endpoints.standardendpoint.log", "promenade.endpoints.standardendpoint.graph", "promenade.endpoints.standardendpoint.property", "ngOrderObjectBy"])
+	.factory("PromenadeStandardEndpoint", ["ParlayEndpoint", PromenadeStandardEndpointFactory])
+	.directive("promenadeStandardEndpointCardToolbar", PromenadeStandardEndpointCardToolbar);

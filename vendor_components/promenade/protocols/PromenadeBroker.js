b15e45646da626733da0a2b85d6d7c428bea1de2
Nick Morris <nick@promenadesoftware.com>
2015-10-26 11:25:21 -0700

    refactor(PromenadeBroker.js): Adding documentation and removing complexity

--- vendor_components/promenade/protocols/PromenadeBroker.js
+++ vendor_components/promenade/protocols/PromenadeBroker.js
@@ -1,213 +1,202 @@
-var broker = angular.module('promenade.broker', ['parlay.socket', 'parlay.notification', 'ngMaterial']);
-
-broker.factory('PromenadeBroker', ['ParlaySocket', '$q', 'ParlayNotification', '$timeout', function (ParlaySocket, $q, ParlayNotification, $timeout) {
+function PromenadeBrokerFactory(ParlaySocket, $q, ParlayNotification, $timeout) {
 	
-    var Public = {};
-    
-    var Private = {
-        connected_previously: false
-    };
-    
-    Public.getBrokerAddress = function () {
-        return ParlaySocket.getAddress();
-    };
-    
-    /**
-     * Requests the ParlaySocket to open itself.
-     * @returns {$q.defer.promise} Requests the ParlaySocket to open.
-     */
-    Public.connect = function () {
-        return ParlaySocket.open();
-    };
-    
-    /**
-     * Requests the ParlaySocket to close itself.
-     * @returns {$q.defer.promise} Requests the ParlaySocket to close.
-     */
-    Public.disconnect = function () {
-        return ParlaySocket.close();
-    };
-    
-    /**
-     * Check status of underlying ParlaySocket.
-     * @returns {Boolean} Requests the ParlaySocket connection status.
-     */
-    Public.isConnected = function () {
-        return ParlaySocket.isConnected();
-    };
-    
-    /**
-     * Requests available protocols for connection from the Broker.
-     * @returns {$q.defer.promise} Resolved with available protocols.
-     */
-    Public.requestAvailableProtocols = function () {
-        return Public.sendRequest('get_protocols', {});
-    };
-    
-    /**
-     * Requests open protocols for connection from the Broker.
-     * @returns {$q.defer.promise} Resolved with open protocols.
-     */
-    Public.requestOpenProtocols = function () {
-        return Public.sendRequest('get_open_protocols', {}).then(function (response) {
-            if (response.status === 'ok') return response.protocols;
-            else Private.handleError(response);            
-        });
-    };
-    
-    /**
-     * Opens protocol.
-     * @param {Object} configuration - Configuration object we should configure a new protocol connection with.
-     * @returns {$q.defer.promise} Resolve when response is received with result of open request from Broker.
-     */
-    Public.openProtocol = function (configuration) {
-        return Public.sendRequest('open_protocol', {'protocol_name': configuration.name, 'params': configuration.parameters}).then(function (response) {
-	        return response.STATUS.toLowerCase().indexOf("error") === -1 ? $q.resolve(response) : $q.reject(response.STATUS);
-        });
-    };
-    
-    /**
-     * Closes protocol.
-     * @param {String} protocol_name - Name of an open protocol.
-     * @returns {$q.defer.promise} Resolve when response is received with result of close request from Broker.
-     */
-    Public.closeProtocol = function (protocol_name) {
-        return Public.sendRequest('close_protocol', {'protocol': protocol_name}).then(function (response) {
-	        return response.STATUS === "ok" ? $q.resolve(response) : $q.reject(response.STATUS);
-        });
-    };
-        
-    /**
-     * Request the Broker for a discovery.
-     * @param {Boolean} is_forced - Force cached invalidation.
-     * @returns {$q.defer.promise} Resolve when response is received with available endpoints.
-     */
-    Public.requestDiscovery = function (is_forced) {
-        
-        // Check we are connected first, otherwise display ParlayNotification.
-        if (!Public.isConnected()) {
+	var broker_instance;
+	
+	function PromenadeBroker() {
+		"use strict";
+	
+	    var connected_previously = false;
+	    
+	    /**
+	     * Sends message to the Broker adding relevant topic fields.
+	     * @param {Object} topics - Map of key/value topic pairs.
+	     * @param {Object} contents - Map of key/value content pairs.
+	     * @param {response_topics} - Map of key/value response topic pairs.
+	     * @returns {$q.defer.promise} Resolve when response is received.
+	     */
+	    this.sendMessage = function(topics, contents, response_topics) {
+	        topics.type = "broker";
+	        response_topics.type = "broker";
+	        
+	        return $q(function (resolve, reject) {
+	            ParlaySocket.sendMessage(topics, contents, response_topics, function (response) { resolve(response); });
+	        });
+	    };
+	    
+	    /**
+		 * Listens for message with relevant response topics from Broker.
+		 * @param {Object} response_topics - Map of key/value response topic pairs.
+		 * @param {Function} response_callback - Function callback to be called on message receipt.
+		 * @returns {Function} - Listener deregistration.
+		 */
+	    this.onMessage = function(response_topics, response_callback) {
+	        response_topics.type = "broker";
+	        
+	        return ParlaySocket.onMessage(response_topics, response_callback);
+	    };
+	    
+	    /**
+	     * Checks if we have connected successfully in the past.
+	     * @returns {Boolean} - True if we have connected successfully, false otherwise.
+	     */
+	    this.hasConnectedPreviously = function() {
+	        return connected_previously;
+	    };
+	    
+	    /**
+		 * Sets our previous connection status to true.
+		 */
+	    this.setConnectedPreviously = function() {
+		    connected_previously = true;
+	    };
+	    
+	    // Actions that PromenadeBroker needs to perform on ParlaySocket open.
+	    // NOTE: Could run into issues if registration occurs after ParlaySocket has been opened. Should investiate solutions.
+		ParlaySocket.onOpen(function () {
+		    
+		    // Request a subscription from the Broker for this protocol.	    
+			ParlaySocket.sendMessage({"type": "subscribe"}, {"TOPICS": {"TO": 61953}});
+	    	ParlaySocket.sendMessage({"type": "subscribe"}, {"TOPICS": {"TO": "UI"}});
+		    
+	        this.setConnectedPreviously();
+	        
+	        ParlayNotification.show({content: "Connected to Parlay Broker!"});
+	        
+	        // Wait for Broker's discovery request.
+	        this.onMessage({command: "get_discovery"}, function(response) {
+		        // Respond with a empty discovery message.
+		        this.sendMessage({response: "get_discovery_response"}, {discovery: []});
+	        });
+	        
+	    }.bind(this));
+	    
+	    // Actions that PromenadeBroker needs to perform on ParlaySocket close.
+	    // NOTE: Could run into issues if registration occurs after ParlaySocket has been opened. Should investiate solutions.
+	    ParlaySocket.onClose(function () {
+		    
+		    // When socket is closed we should show a notification giving the user the option to reconnect.
+		    // If socket failed to open we should show a notification giving the user the option to connect.
+		    ParlayNotification.show(this.hasConnectedPreviously() ? {
+                content: "Disconnected from Parlay Broker!",
+                action: {
+                    text: "Reconnect",
+                    callback: this.connect
+                }
+            } : {
+                content: "Failed to connect to Parlay Broker!",
+                action: {
+                    text: "Connect",
+                    callback: this.connect
+                }
+            });
+	    }.bind(this));
+	    
+	}
+	
+	// Bind ParlaySocket methods to PromenadeBroker.
+    PromenadeBroker.prototype.onMessage = ParlaySocket.onMessage;
+    PromenadeBroker.prototype.onOpen = ParlaySocket.onOpen;
+    PromenadeBroker.prototype.onClose = ParlaySocket.onClose;
+    PromenadeBroker.prototype.getBrokerAddress = ParlaySocket.getAddress;
+	PromenadeBroker.prototype.connect = ParlaySocket.open;
+	PromenadeBroker.prototype.disconnect = ParlaySocket.close;
+	PromenadeBroker.prototype.isConnected = ParlaySocket.isConnected;
+	
+	/**
+	 * Request the Broker for a discovery.
+	 * @param {Boolean} is_forced - Force cached invalidation.
+	 * @returns {$q.defer.promise} Resolve when response is received with available endpoints.
+	 */
+	PromenadeBroker.prototype.requestDiscovery = function (is_forced) {
+	    
+	    // Check we are connected first, otherwise display ParlayNotification.
+	    if (!this.isConnected()) {
 	        ParlayNotification.show({content: "Cannot discover while not connected to Broker."});
+	        
 	        return $q(function (resolve, reject) {
 		        reject("Cannot discover while not connected to Broker.");
 	        });
-        }
-        
-        else {
+	    }
+	    
+	    else {
 	        ParlayNotification.showProgress();
 	        
 	        // Request open protocols before discovery so we know what we're discovering.
-	        return Public.requestOpenProtocols().then(function(open_protocols) {
+	        return this.requestOpenProtocols().then(function(open_protocols) {
 	            return open_protocols;
 	        }).then(function (open_protocols) {
-		    	return Public.sendRequest('get_discovery', {'force': is_forced});
-		    }).then(function (contents) {	            
+		    	return this.sendMessage({request: "get_discovery"}, {"force": is_forced}, {response: "get_discovery_response"});
+		    }.bind(this)).then(function (contents) {	            
 	            var content_string = "Discovered ";
 	            
 	            if (contents.discovery.length === 0) content_string += " Verify connections.";
-	            else if (contents.discovery.length === 1) content_string += contents.discovery[0].NAME + '.';
-	            else content_string += contents.discovery.length + ' protocols.';
+	            else if (contents.discovery.length === 1) content_string += contents.discovery[0].NAME + ".";
+	            else content_string += contents.discovery.length + " protocols.";
 	
 	            ParlayNotification.show({content: content_string});
 	
 	            return contents.discovery;
 	        });    
-        }
-        
-    };
-    
-    /**
-     * Registers a callback on discovery.
-     */
-    Public.onDiscovery = function (callbackFunc) {
-        return Private.onMessage({'response': 'get_discovery_response'}, callbackFunc);
-    };
-    
-    /**
-     * Sends request message to Broker.
-     * @returns {$q.defer.promise} Resolve when response is received.
-     */
-    Public.sendRequest = function (request, contents) {
-        return Private.sendMessage({request: request}, contents, {response: request + '_response'});
-    };
-    
-    /**
-     * Sends message to the Broker.
-     * @returns {$q.defer.promise} Resolve when response is received.
-     */
-    Private.sendMessage = function (topics, contents, response_topics) {
-        topics.type = 'broker';
-        response_topics.type = 'broker';
-        
-        return $q(function (resolve, reject) {
-            ParlaySocket.sendMessage(topics, contents, response_topics, function (response) {
-                resolve(response);
-            });
-        });
-    };
-    
-    Private.onMessage = function (response_topics, response_callback) {
-        response_topics.type = 'broker';
-        
-        return ParlaySocket.onMessage(response_topics, response_callback);
-    };
-    
-    Private.hasConnectedPreviously = function () {
-        return Private.connected_previously;
-    };
-    
-    Private.subscribe = function () {
-	    return $q.all([
-	    	ParlaySocket.sendMessage({'type': 'subscribe'},{"TOPICS": {"TO": 61953}}),
-	    	ParlaySocket.sendMessage({'type': 'subscribe'},{"TOPICS": {"TO": "UI"}})
-	    ]);
-    };
-    
-    Public.onMessage = ParlaySocket.onMessage;
-    Public.onOpen = ParlaySocket.onOpen;
-    Public.onClose = ParlaySocket.onClose;
-    
-    ParlaySocket.onOpen(function () {
+	    }
 	    
-	    // Request a subscription from the Broker for this protocol.	    
-        Private.subscribe();
-	    
-        Private.connected_previously = true;
-        // When socket is opened we should show a ParlayNotification to notify the user.
-        ParlayNotification.show({
-            content: 'Connected to Parlay Broker!'
-        });
-        
-        // Wait for Broker's discovery request.
-        Private.onMessage({command: 'get_discovery'}, function(response) {
-	        // Respond with a empty discovery message.
-	        Private.sendMessage({response:'get_discovery_response'}, {discovery: []});
-        });
-        
-    });
-    
-    Public.onClose(function () {
-        if (Private.hasConnectedPreviously()) {
-            // When socket is closed we should show a notification giving the user the option to reconnect.
-            ParlayNotification.show({
-                content: 'Disconnected from Parlay Broker!',
-                action: {
-                    text: 'Reconnect',
-                    callback: Public.connect
-                }
-            });
-            
-        }
-        else {
-            // If socket failed to open we should show a notification giving the user the option to connect.           
-            ParlayNotification.show({
-                content: 'Failed to connect to Parlay Broker!',
-                action: {
-                    text: 'Connect',
-                    callback: Public.connect
-                }
-            });
-        }
-    });
-    
-    return Public;
-}]);
+	};
+	
+	/**
+	 * Requests available protocols for connection from the Broker.
+	 * @returns {$q.defer.promise} Resolved with available protocols.
+	 */
+	PromenadeBroker.prototype.requestAvailableProtocols = function () {
+	    return this.sendMessage({request: "get_protocols"}, {}, {response: "get_protocols_response"});
+	};
+	
+	/**
+	 * Requests open protocols for connection from the Broker.
+	 * @returns {$q.defer.promise} Resolved with open protocols.
+	 */
+	PromenadeBroker.prototype.requestOpenProtocols = function () {
+	    return this.sendMessage({request: "get_open_protocols"}, {}, {response: "get_open_protocols_response"}).then(function (response) {
+	        if (response.status === "ok") return response.protocols;
+	    });
+	};
+	
+	/**
+	 * Opens protocol.
+	 * @param {Object} configuration - Configuration object we should configure a new protocol connection with.
+	 * @returns {$q.defer.promise} Resolve when response is received with result of open request from Broker.
+	 */
+	PromenadeBroker.prototype.openProtocol = function (configuration) {
+	    return this.sendMessage({request: "open_protocol"}, {"protocol_name": configuration.name, "params": configuration.parameters}, {response: "open_protocol_response"}).then(function (response) {
+	        return response.STATUS.toLowerCase().indexOf("error") === -1 ? $q.resolve(response) : $q.reject(response.STATUS);
+	    });
+	};
+	
+	/**
+	 * Closes protocol.
+	 * @param {String} protocol_name - Name of an open protocol.
+	 * @returns {$q.defer.promise} Resolve when response is received with result of close request from Broker.
+	 */
+	PromenadeBroker.prototype.closeProtocol = function (protocol_name) {
+	    return this.sendMessage({request: "close_protocol"}, {"protocol": protocol_name}, {response: "close_protocol_response"}).then(function (response) {
+	        return response.STATUS === "ok" ? $q.resolve(response) : $q.reject(response.STATUS);
+	    });
+	}; 
+	
+	/**
+	 * Registers a callback on discovery.
+	 * @param {Function} callbackFunc - Callback function to be called on message receipt.
+	 * @returns {Function} - Listener deregistration.
+	 */
+	PromenadeBroker.prototype.onDiscovery = function (callbackFunc) {
+	    return this.onMessage({"response": "get_discovery_response"}, callbackFunc);
+	};
+	
+	// If we don't have an instance of PromenadeBroker instantiated create it and 
+	if (!broker_instance) {
+		broker_instance = new PromenadeBroker();
+	}
+	
+	return broker_instance;
+}
+
+angular.module("promenade.broker", ["parlay.socket", "parlay.notification", "ngMaterial"])
+	.factory("PromenadeBroker", ["ParlaySocket", "$q", "ParlayNotification", "$timeout", PromenadeBrokerFactory]);

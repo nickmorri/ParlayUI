2b1b96d2075ab22bb60236bf91f0c3d58316c282
Nick Morris <nick@promenadesoftware.com>
2015-11-04 13:20:56 -0800

    fix: Removed parlay.navigation dependency

--- app.js
+++ app.js
@@ -1,12 +1,12 @@
 function ParlayConfig($stateProvider, $urlRouterProvider, $mdThemingProvider) {
         
     // Theme configuration
-    $mdThemingProvider.theme('default').primaryPalette('blue-grey').accentPalette('red');
+    $mdThemingProvider.theme("default").primaryPalette("blue-grey").accentPalette("red");
     
     // Default state for unmatched URLs
-    $urlRouterProvider.otherwise('/endpoints');
+    $urlRouterProvider.otherwise("/endpoints");
     
 }
 
-angular.module('parlay.main', ['ui.router', 'parlay.navigation', 'parlay.endpoints'])
+angular.module("parlay.main", ["ui.router", "parlay.endpoints"])
 	.config(ParlayConfig);

b4df6cc85b3603d9a5d13062eb55c2d1868003dc
Nick Morris <nick@promenadesoftware.com>
2015-11-04 13:19:36 -0800

    feat|refactor: Toolbar changes and moving related modules together
    - Toolbar changes to md-warn if not connected to broker.
    - Connections menu text and icon is now static
    - Moved related modules together for simplicity

--- parlay_components/endpoints/endpoints.js
+++ parlay_components/endpoints/endpoints.js
@@ -1,28 +1,98 @@
 function EndpointsConfiguration($stateProvider) {
-    $stateProvider.state('endpoints', {
-        url: '/endpoints',
-        templateUrl: '../parlay_components/endpoints/views/base.html',
-        controller: 'ParlayEndpointController'
+    $stateProvider.state("endpoints", {
+        url: "/endpoints",
+        templateUrl: "../parlay_components/endpoints/views/base.html",
+        controller: "ParlayEndpointController",
+        controllerAs: "endpointCtrl"
     });
 }
 
-function ParlayEndpointsToolbar($mdMedia) {
+function ParlayEndpointController($scope, ParlayEndpointManager) {
+	
+	this.filterEndpoints = function () {
+        return ParlayEndpointManager.getActiveEndpoints();
+    };
+    
+    this.hasEndpoints = function () {
+	    return ParlayEndpointManager.hasActiveEndpoints();
+    };
+    
+    this.reorder = function (index, distance) {
+	    ParlayEndpointManager.reorder(parseInt(index, 10), distance);
+    };
+    
+    this.duplicate = function (index, uid) {
+	    ParlayEndpointManager.duplicateEndpoint(parseInt(index, 10), uid);
+    };
+    
+    this.deactivate = function (index) {
+	    ParlayEndpointManager.deactivateEndpoint(parseInt(index, 10));
+    };
+	    
+}
+
+function ParlayEndpointsToolbarController($scope, $mdDialog, $mdSidenav, ParlayEndpointManager) {
+	
+	this.openSidenav = function () {
+		/* istanbul ignore next */
+		$mdSidenav("navigation").open();
+	};
+	
+	this.requestDiscovery = function () {
+        ParlayEndpointManager.requestDiscovery();
+    };
+    
+    this.openWorkspaceManagementDialog = function (event) {
+	    /* istanbul ignore next */
+	    $mdDialog.show({
+		    controller: "ParlayWorkspaceManagementController",
+		    templateUrl: "../parlay_components/endpoints/directives/parlay-workspace-management-dialog.html",
+		    targetEvent: event,
+		    clickOutsideToClose: true
+	    });
+    };
+    
+    this.viewConnections = function (event) {
+	    /* istanbul ignore next */
+        $mdDialog.show({
+            targetEvent: event,
+            clickOutsideToClose: true,
+            controller: "ParlayConnectionListController",
+            templateUrl: "../parlay_components/communication/directives/parlay-connection-list-dialog.html"
+        });
+    };
+    
+}
+
+function ParlayEndpointsToolbar($mdMedia, PromenadeBroker) {
     return {
+	    scope: {},
 	    replace: true,
-        templateUrl: '../parlay_components/endpoints/directives/parlay-endpoints-toolbar.html',
+        templateUrl: "../parlay_components/endpoints/directives/parlay-endpoints-toolbar.html",
         link: function ($scope, element, attributes) {
+	        
+	        // Watch our connection status to Broker and update the icon if connectivity changes.
+	        $scope.$watch(function () {
+                return PromenadeBroker.isConnected();
+            }, function (connected) {
+	            $scope.connected = connected;
+            });
 	        
 	        // Watch the size of the screen, if we are on a screen size that's greater than a small screen we should always display labels.
             $scope.$watch(function () {
-	            return $mdMedia('gt-md');
+	            return $mdMedia("gt-md");
             }, function (large_screen) {
 	            $scope.large_screen = large_screen;
             });
             
-        }
+        },
+        controller: "ParlayEndpointsToolbarController",
+        controllerAs: "ctrl"
     };
 }
 
-angular.module('parlay.endpoints', ['ui.router', 'parlay.endpoints.manager', 'parlay.endpoints.search', 'parlay.endpoints.endpoint', 'parlay.endpoints.controller', "parlay.sidenav"])
+angular.module("parlay.endpoints", ["ui.router", "parlay.endpoints.manager", "parlay.endpoints.search", "parlay.endpoints.endpoint", "parlay.protocols.list_controller", "parlay.sidenav"])
 	.config(["$stateProvider", EndpointsConfiguration])
-	.directive('parlayEndpointsToolbar', ['$mdMedia', ParlayEndpointsToolbar]);
+	.controller("ParlayEndpointsToolbarController", ["$scope", "$mdDialog", "$mdSidenav", "ParlayEndpointManager", ParlayEndpointsToolbarController])
+	.controller("ParlayEndpointController", ["$scope", "ParlayEndpointManager", ParlayEndpointController])
+	.directive("parlayEndpointsToolbar", ["$mdMedia", "PromenadeBroker", ParlayEndpointsToolbar]);

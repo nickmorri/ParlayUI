4b9f7b3513770eb1c6d0a699caf37ba16ef1f273
Nick Morris <nick@promenadesoftware.com>
2015-11-05 10:00:58 -0800

    refactor: Move ParlayEndpointsToolbar to seperate file

--- parlay_components/endpoints/endpoints.js
+++ parlay_components/endpoints/endpoints.js
@@ -3,7 +3,11 @@ function EndpointsConfiguration($stateProvider) {
         url: "/endpoints",
         templateUrl: "../parlay_components/endpoints/views/base.html",
         controller: "ParlayEndpointController",
-        controllerAs: "endpointCtrl"
+        controllerAs: "endpointCtrl",
+        data: {
+	        display: "Endpoints",
+			icon: "dashboard"
+		}
     });
 }
 
@@ -31,68 +35,6 @@ function ParlayEndpointController($scope, ParlayEndpointManager) {
 	    
 }
 
-function ParlayEndpointsToolbarController($scope, $mdDialog, $mdSidenav, ParlayEndpointManager) {
-	
-	this.openSidenav = function () {
-		/* istanbul ignore next */
-		$mdSidenav("navigation").open();
-	};
-	
-	this.requestDiscovery = function () {
-        ParlayEndpointManager.requestDiscovery();
-    };
-    
-    this.openWorkspaceManagementDialog = function (event) {
-	    /* istanbul ignore next */
-	    $mdDialog.show({
-		    templateUrl: "../parlay_components/endpoints/directives/parlay-workspace-management-dialog.html",
-		    targetEvent: event,
-		    controller: "ParlayWorkspaceManagementController",
-		    clickOutsideToClose: true
-	    });
-    };
-    
-    this.openConnectionManagementDialog = function (event) {
-	    /* istanbul ignore next */
-        $mdDialog.show({
-            templateUrl: "../parlay_components/communication/directives/parlay-connection-list-dialog.html",
-            targetEvent: event,
-            controller: "ParlayConnectionListController",
-            clickOutsideToClose: true
-        });
-    };
-    
-}
-
-function ParlayEndpointsToolbar($mdMedia, PromenadeBroker) {
-    return {
-	    scope: {},
-	    replace: true,
-        templateUrl: "../parlay_components/endpoints/directives/parlay-endpoints-toolbar.html",
-        link: function ($scope, element, attributes) {
-	        
-	        // Watch our connection status to Broker and update the icon if connectivity changes.
-	        $scope.$watch(function () {
-                return PromenadeBroker.isConnected();
-            }, function (connected) {
-	            $scope.connected = connected;
-            });
-	        
-	        // Watch the size of the screen, if we are on a screen size that's greater than a small screen we should always display labels.
-            $scope.$watch(function () {
-	            return $mdMedia("gt-md");
-            }, function (large_screen) {
-	            $scope.large_screen = large_screen;
-            });
-            
-        },
-        controller: "ParlayEndpointsToolbarController",
-        controllerAs: "ctrl"
-    };
-}
-
-angular.module("parlay.endpoints", ["ui.router", "parlay.endpoints.manager", "parlay.endpoints.search", "parlay.endpoints.endpoint", "parlay.protocols.list_controller", "parlay.sidenav"])
+angular.module("parlay.endpoints", ["ui.router", "parlay.endpoints.manager", "parlay.endpoints.toolbar"])
 	.config(["$stateProvider", EndpointsConfiguration])
-	.controller("ParlayEndpointsToolbarController", ["$scope", "$mdDialog", "$mdSidenav", "ParlayEndpointManager", ParlayEndpointsToolbarController])
-	.controller("ParlayEndpointController", ["$scope", "ParlayEndpointManager", ParlayEndpointController])
-	.directive("parlayEndpointsToolbar", ["$mdMedia", "PromenadeBroker", ParlayEndpointsToolbar]);
+	.controller("ParlayEndpointController", ["$scope", "ParlayEndpointManager", ParlayEndpointController]);

2938c81a21733d06798d3ed7db204207c0903c5e
Nick Morris <nick@promenadesoftware.com>
2015-11-09 10:16:10 -0800

    fix: Meeting new angular-material spec

--- parlay_components/protocols/test/ParlayProtocolConfigurationController.js
+++ parlay_components/protocols/test/ParlayProtocolConfigurationController.js
@@ -1,79 +1,62 @@
 (function () {
-    'use strict';
+    "use strict";
     
-    describe('parlay.protocols.configuration_controller', function() {
+    describe("parlay.protocols.configuration_controller", function() {
     
-    	beforeEach(module('parlay.protocols.configuration_controller'));
-    	beforeEach(module('mock.parlay.protocols.manager'));
+    	beforeEach(module("parlay.protocols.configuration_controller"));
+    	beforeEach(module("mock.parlay.protocols.manager"));
         
-        describe('ProtocolConfigurationController', function () {
-                var rootScope, scope, ParlayProtocolConfigurationController;                
+        describe("ProtocolConfigurationController", function () {
+                var rootScope, scope, ctrl;                
                 
                 beforeEach(inject(function ($rootScope, $controller, $q) {
                     rootScope = $rootScope;
                     scope = $rootScope.$new();
-                    ParlayProtocolConfigurationController = $controller('ParlayProtocolConfigurationController', {$scope: scope});
+                    ctrl = $controller("ParlayProtocolConfigurationController", {$scope: scope});
                 }));
                 
-                it('initial state', function () {
-                    expect(scope.selected_protocol).toBe(null);
-                    expect(scope.connecting).toBeFalsy();
+                it("initial state", function () {
+                    expect(ctrl.selected_protocol).toBe(null);
+                    expect(ctrl.connecting).toBeFalsy();
                 });
                 
-                it('searches for available protocols', function() {
-                    expect(scope.filterProtocols('')).toEqual([
-                        {
-                            name: 'BarProtocol'
-                        },
-                        {
-                            name: 'FooProtocol'
-                        }
-                    ]);
-                    expect(scope.filterProtocols('foo')).toEqual([
-                        {
-                            name: 'FooProtocol'
-                        }
-                    ]);
+                it("searches for available protocols", function() {
+                    expect(ctrl.filterProtocols("")).toEqual([ { name: "BarProtocol" }, { name: "FooProtocol" } ]);
+                    expect(ctrl.filterProtocols("foo")).toEqual([ { name: "FooProtocol" } ]);
                 });
                 
-                it('searches for default parameters', function () {
-                    expect(scope.filterDefaults(['test', 'foo', 'bar', 'foobar'], '')).toEqual(['test', 'foo', 'bar', 'foobar']);
-                    expect(scope.filterDefaults(['test', 'foo', 'bar', 'foobar'], 'foo')).toEqual(['foo', 'foobar']);
+                it("searches for default parameters", function () {
+                    expect(ctrl.filterDefaults(["test", "foo", "bar", "foobar"], "")).toEqual(["test", "foo", "bar", "foobar"]);
+                    expect(ctrl.filterDefaults(["test", "foo", "bar", "foobar"], "foo")).toEqual(["foo", "foobar"]);
                 });
                 
-                it('checks if selected protocol has parameters', function() {
-                    scope.selected_protocol = {parameters: ['test', 'foo', 'bar']};
-                    expect(scope.selectedProtocolHasParameters()).toBeTruthy();
+                it("checks if selected protocol has parameters", function() {
+                    ctrl.selected_protocol = { name: "SuccessfulProtocol", parameters: {"test": {value: 1}, "foo": {value: 2}, "bar": {value: 3}} };
+                    expect(ctrl.selectedProtocolHasParameters()).toBeTruthy();
                 });
                 
-                it('connects a configured protocol', function () {
-                    expect(scope.connecting).toBeFalsy();
+                it("connects a configured protocol", function () {
+                    expect(ctrl.connecting).toBeFalsy();
                     
-                    scope.selected_protocol = {
-                        name: 'SuccessfulProtocol',
-                        parameters: ['test', 'foo', 'bar']
-                    };
+                    ctrl.selected_protocol = { name: "SuccessfulProtocol", parameters: {"test": {value: 1}, "foo": {value: 2}, "bar": {value: 3}} };
                     
-                    scope.connect();
+                    ctrl.connect();
                     rootScope.$apply();
                     
-                    expect(scope.connecting).toBeTruthy();
+                    expect(ctrl.connecting).toBeTruthy();
                 });
                 
-                it('fails to connect a protocol', function () {
-                    expect(scope.connecting).toBeFalsy();
+                it("fails to connect a protocol", function () {
+                    expect(ctrl.connecting).toBeFalsy();
                     
-                    scope.selected_protocol = {
-                        name: 'FailureProtocol',
-                        parameters: ['test', 'foo', 'bar']
-                    };
+                    ctrl.selected_protocol = { name: "FailureProtocol", parameters: {"test": {value: 1}, "foo": {value: 2}, "bar": {value: 3}} };
                     
-                    scope.connect();
+                    ctrl.connect();
                     rootScope.$apply();
                     
-                    expect(scope.connecting).toBeFalsy();
-                    expect(scope.error).toBeTruthy();
-                    expect(scope.error_message).toBe('error');
+                    expect(ctrl.connecting).toBeFalsy();
+                    expect(ctrl.error).toBeTruthy();
+                    expect(ctrl.error_message).toBe("error");
                 });
                 
             });

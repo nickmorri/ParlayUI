f1fb0bc53b62a8991480a428bd8dabfa297f5b77
Nick Morris <nick@promenadesoftware.com>
2015-11-09 14:54:42 -0800

    feat: Using JS class rather than object extension

--- parlay_components/endpoints/ParlayEndpointManager.js
+++ parlay_components/endpoints/ParlayEndpointManager.js
@@ -1,48 +1,48 @@
-function ParlayEndpointManager(PromenadeBroker, ParlayProtocolManager, ParlayNotification, ParlayStore, $window) {
+function ParlayEndpointManagerFactory(PromenadeBroker, ParlayProtocolManager, ParlayNotification, ParlayStore, $window) {
     
-    var store = ParlayStore("endpoints");
+    var instance;
     
-    var has_discovered = false;
-	var active_endpoints = [];
-    
-    var Public = {};
+    function ParlayEndpointManager() {
+	    this.has_discovered = false;
+	    this.active_endpoints = [];
+    }
     
     /**
 	 * Returns Object of active endpoint containers
 	 * @returns {Object} key: order, value: active endpoint containers
 	 */
-    Public.getActiveEndpoints = function () {
-        return active_endpoints;
+    ParlayEndpointManager.prototype.getActiveEndpoints = function () {
+	    return this.active_endpoints;
     };
     
     /**
 	 * Checks if we currently have active endpoints.
 	 * @returns {Boolean} true if we have endpoints, false otherwise.
 	 */
-    Public.hasActiveEndpoints = function () {
-	    return Public.getActiveEndpointCount() > 0;
+    ParlayEndpointManager.prototype.hasActiveEndpoints = function () {
+	    return this.getActiveEndpointCount() > 0;
     };
     
     /**
 	 * Returns the number of endpoints currently active.
 	 * @returns {Number} endpoint count
 	 */
-    Public.getActiveEndpointCount = function () {
-	    return Public.getActiveEndpoints().length;
+    ParlayEndpointManager.prototype.getActiveEndpointCount = function () {
+	    return this.getActiveEndpoints().length;
     };
     
     /**
 	 * Clears reference to active endpoint object.
 	 */
-    Public.clearActiveEndpoints = function () {
-	    active_endpoints = [];
+    ParlayEndpointManager.prototype.clearActiveEndpoints = function () {
+	    this.active_endpoints = [];
     };
     
     /**
 	 * Returns the available endpoints from all connected protocols.
 	 * @returns {Array} endpoints available on all protocols
 	 */
-    Public.getAvailableEndpoints = function () {
+    ParlayEndpointManager.prototype.getAvailableEndpoints = function () {
         return ParlayProtocolManager.getOpenProtocols().reduce(function (previous, current) {
             return previous.concat(current.getAvailableEndpoints());
         }, []);
@@ -52,19 +52,19 @@ function ParlayEndpointManager(PromenadeBroker, ParlayProtocolManager, ParlayNot
 	 * Check whether any discovery request has been made.
 	 * @returns {Boolean} - True if we have successfully completed a discovery, false otherwise.
 	 */
-    Public.hasDiscovered = function () {
-	    return has_discovered;
+    ParlayEndpointManager.prototype.hasDiscovered = function () {
+	    return this.has_discovered;
     };
     
     /**
 	 * Requests discovery from PromenadeBroker.
 	 * @returns {$q.defer.promise} Resolved when a response is received from the Broker.
 	 */
-    Public.requestDiscovery = function () {
+    ParlayEndpointManager.prototype.requestDiscovery = function () {
         return PromenadeBroker.requestDiscovery(true).then(function(response) {
-	        has_discovered = true;
+	        this.has_discovered = true;
 	        return response;
-        });
+        }.bind(this));
     };
     
     /**
@@ -72,10 +72,10 @@ function ParlayEndpointManager(PromenadeBroker, ParlayProtocolManager, ParlayNot
 	 * @param {Number} index - position of endpoint to swap.
 	 * @param {Number} distance - how far to move target endpoint.
 	 */
-    Public.reorder = function (index, distance) {
-	    var temp = active_endpoints[index + distance];
-	    active_endpoints[index + distance] = active_endpoints[index];
-	    active_endpoints[index] = temp;
+    ParlayEndpointManager.prototype.reorder = function (index, distance) {
+	    var temp = this.active_endpoints[index + distance];
+	    this.active_endpoints[index + distance] = this.active_endpoints[index];
+	    this.active_endpoints[index] = temp;
     };
     
     /**
@@ -83,8 +83,8 @@ function ParlayEndpointManager(PromenadeBroker, ParlayProtocolManager, ParlayNot
 	 * @param {ParlayEndpoint} endpoint - reference to the endpoint object we want to activate.
 	 * @param {Number} uid[optional] - If given a uid we will use the provided one. Otherwise we will randomly generate one.
 	 */
-    Public.activateEndpoint = function (endpoint, uid) {
-	    active_endpoints.push({
+    ParlayEndpointManager.prototype.activateEndpoint = function (endpoint, uid) {
+	    this.active_endpoints.push({
 		    ref: endpoint,
 		    uid: uid !== undefined ? uid : Math.floor(Math.random() * 1500)
 	    });
@@ -94,38 +94,38 @@ function ParlayEndpointManager(PromenadeBroker, ParlayProtocolManager, ParlayNot
 	 * Creates a duplicate endpoint container that references the same endpoint available at the given index.
 	 * @param {Number} index - position of the endpoint we want to duplicate.
 	 */
-    Public.duplicateEndpoint = function (index) {
-	    var container = active_endpoints[index];
+    ParlayEndpointManager.prototype.duplicateEndpoint = function (index) {
+	    var container = this.active_endpoints[index];
 	    var new_uid = container.uid + Math.floor(Math.random() * 1500);
 	    
 	    var old_directive = 'parlayEndpointCard.' + container.ref.name.replace(' ', '_') + '_' + container.uid;
 	    var new_directive = 'parlayEndpointCard.' + container.ref.name.replace(' ', '_') + '_' + new_uid;
 	    
 		// We should remove and Angular $ or $$ variables since they are generated.
-		var session_data = store.getSessionItem(old_directive);
+		var session_data = ParlayStore("endpoints").getSessionItem(old_directive);
 		for (var key in session_data) {
 			if (key.indexOf('$') !== -1) {
 				delete session_data[key];
 			}
 		}
 	    
-	    store.setSessionItem(new_directive, session_data);
-	    Public.activateEndpoint(container.ref, new_uid);
+	    ParlayStore("endpoints").setSessionItem(new_directive, session_data);
+	    this.activateEndpoint(container.ref, new_uid);
     };
     
     /**
 	 * Deactivates an endoint that is currently active.
 	 * @param {Number} index - position of endpoint to be deactivated.
 	 */
-    Public.deactivateEndpoint = function (index) {
-	    active_endpoints.splice(index, 1);
+    ParlayEndpointManager.prototype.deactivateEndpoint = function (index) {
+	    this.active_endpoints.splice(index, 1);
     };
     
     /**
 	 * Loads endpoints from the specified workspace.
 	 * @param {Workspace} workspace - saved workspace to be loaded.
 	 */
-	Public.loadWorkspace = function (workspace) {
+	ParlayEndpointManager.prototype.loadWorkspace = function (workspace) {
 		
 		// Sort by the $index recorded from the previous session. This corresponds with the order that the cards will be loaded into the workspace.
 		var containers = Object.keys(workspace.data).sort(function (a, b) {
@@ -144,31 +144,35 @@ function ParlayEndpointManager(PromenadeBroker, ParlayProtocolManager, ParlayNot
 		
 		// Add each saved card to the workspace if their exists a valid available endpoint.
 		containers.forEach(function (container) {
-			var endpoint = Public.getAvailableEndpoints().find(function (endpoint) {
+			var endpoint = this.getAvailableEndpoints().find(function (endpoint) {
 				return container.name === endpoint.name;
 			});
 			if (endpoint !== undefined) {
 				loaded_endpoints = true;
-				Public.activateEndpoint(endpoint, container.uid);
+				this.activateEndpoint(endpoint, container.uid);
 			}
-		});
+		}, this);
 		
 		return loaded_endpoints;
 		
 	};
 	
-	Public.autoSave = function() {
-		if (Public.hasActiveEndpoints()) ParlayStore("endpoints").moveItemToLocal('AutoSave', true);
+	ParlayEndpointManager.prototype.autoSave = function() {
+		if (this.hasActiveEndpoints()) ParlayStore("endpoints").moveItemToLocal('AutoSave', true);
 	};
 	
 	PromenadeBroker.onClose(function () {
 		has_discovered = false;
 	});
 	
-	$window.onbeforeunload = Public.autoSave;
+	$window.onbeforeunload = ParlayEndpointManager.prototype.autoSave;
 	
-	return Public;
+	if (!instance) {
+		instance = new ParlayEndpointManager();
+	}
+	
+	return instance;
 }
 
 angular.module('parlay.endpoints.manager', ['parlay.protocols.manager', 'promenade.broker', 'parlay.store', 'parlay.endpoints.workspaces'])
-	.factory('ParlayEndpointManager', ['PromenadeBroker', 'ParlayProtocolManager', 'ParlayNotification', 'ParlayStore', '$window', ParlayEndpointManager]);
+	.factory('ParlayEndpointManager', ['PromenadeBroker', 'ParlayProtocolManager', 'ParlayNotification', 'ParlayStore', '$window', ParlayEndpointManagerFactory]);

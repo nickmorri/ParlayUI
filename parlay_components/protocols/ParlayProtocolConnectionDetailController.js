f4da4a36888ede706daf5c78311245d1761bda9b
Nick Morris <nick@promenadesoftware.com>
2015-11-11 11:45:00 -0800

    doc: Added docstrings

--- parlay_components/protocols/ParlayProtocolConnectionDetailController.js
+++ parlay_components/protocols/ParlayProtocolConnectionDetailController.js
@@ -1,14 +1,25 @@
 function ParlayProtocolConnectionDetailController($mdDialog, protocol) {
     "use strict";
 
+    /**
+     * Returns the name of the protocol.
+     * @returns {String} - Protocol name.
+     */
     this.getProtocolName = function () {
         return protocol.getName();
     };
-    
+
+    /**
+     * Returns an Array of collected log messages.
+     * @returns {Array} - Log messages.
+     */
     this.getLog = function () {
         return protocol.getLog();
     };
-    
+
+    /**
+     * Hides the $mdDialog
+     */
     this.hide = function () {
         $mdDialog.hide();
     };

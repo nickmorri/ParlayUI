f2c943ab57fe07546e080bc38696aba3e593155b
Nick Morris <nick@promenadesoftware.com>
2016-01-26 18:37:09 -0800

    feat: ParlayErrorDialog contents is collapsed by default

--- parlay_components/notification/ParlayErrorDialog.js
+++ parlay_components/notification/ParlayErrorDialog.js
@@ -25,6 +25,35 @@ function ParlayErrorDialog($mdDialog, $mdMedia, ParlayNotificationHistory) {
 
 function ParlayErrorDialogController($mdDialog) {
 
+    // Holds state of topics visibility and the corresponding icon to indicate this state to the user.
+    this.topics_hidden = false;
+    this.topics_icon = "arrow_drop_down";
+
+    /**
+     * Toggles state of topics visibility.
+     */
+    this.toggleTopics = function () {
+        this.topics_hidden = !this.topics_hidden;
+        if (this.topics_hidden) this.topics_icon = "arrow_drop_down";
+        else this.topics_icon = "arrow_drop_up";
+    };
+
+    // Holds state of contents visibility and the corresponding icon to indicate this state to the user.
+    this.contents_hidden = true;
+    this.contents_icon = "arrow_drop_down";
+
+    /**
+     * Toggles state of contents visibility.
+     */
+    this.toggleContents = function () {
+        this.contents_hidden = !this.contents_hidden;
+        if (this.contents_hidden) this.contents_icon = "arrow_drop_down";
+        else this.contents_icon = "arrow_drop_up";
+    };
+
+    /**
+     * Hides $mdDialog, this will resolve the function that launched this dialog.
+     */
     this.close = function () {
         $mdDialog.hide();
     };

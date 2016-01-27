c9c929ef11c2af54318f94efee6d8e03d7100dcd
Nick Morris <nick@promenadesoftware.com>
2016-01-27 13:56:03 -0800

    feat: ParlayErrorDialog displays more immediately relevant information

--- parlay_components/notification/ParlayErrorDialog.js
+++ parlay_components/notification/ParlayErrorDialog.js
@@ -25,30 +25,17 @@ function ParlayErrorDialog($mdDialog, $mdMedia, ParlayNotificationHistory) {
 
 function ParlayErrorDialogController($mdDialog) {
 
-    // Holds state of topics visibility and the corresponding icon to indicate this state to the user.
-    this.topics_hidden = true;
-    this.topics_icon = "arrow_drop_down";
+    // Holds state of more detail visibility and the corresponding icon to indicate this state to the user.
+    this.more_hidden = true;
+    this.more_icon = "arrow_drop_down";
 
     /**
-     * Toggles state of topics visibility.
+     * Toggles state of more details visibility.
      */
-    this.toggleTopics = function () {
-        this.topics_hidden = !this.topics_hidden;
-        if (this.topics_hidden) this.topics_icon = "arrow_drop_down";
-        else this.topics_icon = "arrow_drop_up";
-    };
-
-    // Holds state of contents visibility and the corresponding icon to indicate this state to the user.
-    this.contents_hidden = true;
-    this.contents_icon = "arrow_drop_down";
-
-    /**
-     * Toggles state of contents visibility.
-     */
-    this.toggleContents = function () {
-        this.contents_hidden = !this.contents_hidden;
-        if (this.contents_hidden) this.contents_icon = "arrow_drop_down";
-        else this.contents_icon = "arrow_drop_up";
+    this.toggleMore = function () {
+        this.more_hidden = !this.more_hidden;
+        if (this.more_hidden) this.more_icon = "arrow_drop_down";
+        else this.more_icon = "arrow_drop_up";
     };
 
     /**

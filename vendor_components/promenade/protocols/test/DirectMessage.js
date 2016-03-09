12f030cd5a0a6a957822f043c63c6bb6d1e7c898
Nick Morris <nickmorri@gmail.com>
2016-03-09 13:23:27 -0800

    test: Implemented DirectMessage test suite

--- vendor_components/promenade/protocols/test/DirectMessage.js
+++ vendor_components/promenade/protocols/test/DirectMessage.js
@@ -4,13 +4,15 @@
     describe('promenade.protocols.directmessage', function() {
     
         beforeEach(module('promenade.protocols.directmessage'));
+        beforeEach(module('mock.parlay.socket'));
         
         describe('PromenadeDirectMessageProtocol', function() {
-            var rootScope, protocol, socket;
+            var $rootScope, protocol, socket, $q;
             
-            beforeEach(inject(function($rootScope, _PromenadeDirectMessageProtocol_, _ParlaySocket_) {
+            beforeEach(inject(function(_$rootScope_, _$q_,  _PromenadeDirectMessageProtocol_, _ParlaySocket_) {
                 /*jshint newcap: false */
-                rootScope = $rootScope;
+                $rootScope = _$rootScope_;
+                $q = _$q_;
                 protocol = new _PromenadeDirectMessageProtocol_({name: 'TestProtocol', protocol_type: 'TestProtocolType'});
                 socket = _ParlaySocket_;
             }));
@@ -51,25 +53,58 @@
                 });
             });
             
-            it('sends a message', function () {
-                
-                spyOn(socket, 'sendMessage');
-                
-                protocol.sendMessage({
-                    TO: 100
-                }, {}, {
-	                FROM: 100
+            describe('sends a message', function () {
+
+                it("resolves", function (done) {
+                    spyOn(socket, 'sendMessage').and.callThrough();
+                    spyOn(protocol, "buildMessageTopics").and.callThrough();
+                    spyOn(protocol, "buildResponseTopics").and.callThrough();
+
+                    protocol.sendMessage({TO: 100}, {}, {FROM: 100}, false).then(done);
+
+                    $rootScope.$apply();
+
+                    expect(protocol.buildMessageTopics).toHaveBeenCalledWith({TO: 100});
+                    expect(protocol.buildResponseTopics).toHaveBeenCalledWith({TO: 100, MSG_ID: 201, FROM: 'UI'}, {FROM: 100}, false);
+
+                    expect(socket.sendMessage).toHaveBeenCalledWith(jasmine.objectContaining({
+                            TO: 100,
+                            FROM: "UI",
+                            MSG_ID: protocol.getMessageId(),
+                        }), jasmine.objectContaining({}), jasmine.objectContaining({
+                            FROM: 100
+                        }), jasmine.any(Function),
+                        true);
+                });
+
+                it("rejects", function (done) {
+                    spyOn(socket, 'sendMessage').and.callThrough();
+                    spyOn(protocol, "buildMessageTopics").and.callThrough();
+                    spyOn(protocol, "buildResponseTopics").and.callThrough();
+
+                    protocol.sendMessage({TO: 100}, {}, {FROM: 200}, false).catch(done);
+
+                    $rootScope.$apply();
+
+                    expect(protocol.buildMessageTopics).toHaveBeenCalledWith({TO: 100});
+                    expect(protocol.buildResponseTopics).toHaveBeenCalledWith({TO: 100, MSG_ID: 201, FROM: 'UI'}, {FROM: 200}, false);
+
+                    expect(socket.sendMessage).toHaveBeenCalledWith(jasmine.objectContaining({
+                            TO: 100,
+                            FROM: "UI",
+                            MSG_ID: protocol.getMessageId(),
+                        }), jasmine.objectContaining({}), jasmine.objectContaining({
+                            FROM: 200
+                        }), jasmine.any(Function),
+                        true);
                 });
                 
-                expect(socket.sendMessage).toHaveBeenCalledWith(jasmine.objectContaining({
-	                TO: 100,
-                    FROM: "UI",
-                    MSG_ID: protocol.getMessageId(),
-                }), jasmine.objectContaining({}), jasmine.objectContaining({
-					FROM: 100,
-                }), jasmine.any(Function),
-                true);
-                
+            });
+
+            it("checks if response is ok", function () {
+                expect(protocol.isResponseOk({TOPICS: {MSG_STATUS: "ERROR"}})).toBeFalsy();
+                expect(protocol.isResponseOk({TOPICS: {MSG_STATUS: "WARNING"}})).toBeFalsy();
+                expect(protocol.isResponseOk({TOPICS: {MSG_STATUS: "OK"}})).toBeTruthy();
             });
 
         });

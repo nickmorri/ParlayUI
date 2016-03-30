170e545c649593cc2d0a0168e877a9d1b64a0e23
Nick Morris <nickmorri@gmail.com>
2016-03-30 09:44:12 -0700

    refactor: Removed unused dependency injection

--- app.js
+++ app.js
@@ -1,15 +1,15 @@
-function ParlayConfig($stateProvider, $urlRouterProvider, $mdThemingProvider) {
+function ParlayConfig($urlRouterProvider, $mdThemingProvider) {
         
-    // Theme configuration
+    // Theme configuration.
     $mdThemingProvider.theme("default").primaryPalette("blue-grey").accentPalette("red");
 
     // Needed for warning toast.
     $mdThemingProvider.theme("warning-toast").primaryPalette("amber");
 
-    // Default state for unmatched URLs
+    // Default state for unmatched URLs.
     $urlRouterProvider.otherwise("/items");
     
 }
 
-angular.module("parlay.main", ["ui.router", "ngMaterial", "parlay.items", "parlay.navigation.container", "parlay.notification.sidenav"])
+angular.module("parlay.main", ["ui.router", "ngMaterial", "parlay.items", "parlay.editor", "parlay.navigation.container", "parlay.notification.sidenav"])
 	.config(ParlayConfig);

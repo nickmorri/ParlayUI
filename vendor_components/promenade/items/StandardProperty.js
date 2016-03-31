dc491150ee3627924e435cd801849b50f1991581
Nick Morris <nickmorri@gmail.com>
2016-03-30 17:43:07 -0700

    fix: Indentation

--- vendor_components/promenade/items/StandardProperty.js
+++ vendor_components/promenade/items/StandardProperty.js
@@ -11,21 +11,21 @@ function PromenadeStandardPropertyFactory() {
 
         this.get = function () {
             return protocol.sendMessage({
-                    TX_TYPE: "DIRECT",
-                    MSG_TYPE: "PROPERTY",
-                    TO: this.item_name
-                },
-                {
-                    PROPERTY: this.name,
-                    ACTION: "GET",
-                    VALUE: null
-                },
-                {
-                    TX_TYPE: "DIRECT",
-                    MSG_TYPE: "RESPONSE",
-                    FROM: this.item_name,
-                    TO: "UI"
-                }, true).then(function(response) {
+                TX_TYPE: "DIRECT",
+                MSG_TYPE: "PROPERTY",
+                TO: this.item_name
+            },
+            {
+                PROPERTY: this.name,
+                ACTION: "GET",
+                VALUE: null
+            },
+            {
+                TX_TYPE: "DIRECT",
+                MSG_TYPE: "RESPONSE",
+                FROM: this.item_name,
+                TO: "UI"
+            }, true).then(function(response) {
                 this.value = response.CONTENTS.VALUE;
                 return response;
             }.bind(this));
@@ -33,21 +33,21 @@ function PromenadeStandardPropertyFactory() {
 
         this.set = function () {
             return protocol.sendMessage({
-                    TX_TYPE: "DIRECT",
-                    MSG_TYPE: "PROPERTY",
-                    TO: this.item_name
-                },
-                {
-                    PROPERTY: this.name,
-                    ACTION: "SET",
-                    VALUE: this.value
-                },
-                {
-                    TX_TYPE: "DIRECT",
-                    MSG_TYPE: "RESPONSE",
-                    FROM: this.item_name,
-                    TO: "UI"
-                }, true);
+                TX_TYPE: "DIRECT",
+                MSG_TYPE: "PROPERTY",
+                TO: this.item_name
+            },
+            {
+                PROPERTY: this.name,
+                ACTION: "SET",
+                VALUE: this.value
+            },
+            {
+                TX_TYPE: "DIRECT",
+                MSG_TYPE: "RESPONSE",
+                FROM: this.item_name,
+                TO: "UI"
+            }, true);
         };
     }
 

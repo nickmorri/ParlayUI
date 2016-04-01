05da9f3a0a8390873a525202bcc1bf94bb89854a
Nick Morris <nickmorri@gmail.com>
2016-03-31 17:25:47 -0700

    test: Improved ParlayProtocolListController coverage

--- parlay_components/protocols/test/ParlayProtocolListController.js
+++ parlay_components/protocols/test/ParlayProtocolListController.js
@@ -6,9 +6,9 @@
         beforeEach(module('parlay.protocols.list_controller'));
         
         describe('ParlayProtocolListController', function () {
-            var rootScope, scope, ctrl, MockPromenadeBroker;                
+            var rootScope, scope, ctrl, MockPromenadeBroker, ParlayProtocolManager;
             
-            beforeEach(inject(function ($rootScope, $controller) {
+            beforeEach(inject(function ($rootScope, $controller, $q, _ParlayProtocolManager_) {
                 
                 MockPromenadeBroker = {
                     connected: false,
@@ -29,10 +29,16 @@
                         this.connected = false;
                     }
                 };
-                
+
+                ParlayProtocolManager = _ParlayProtocolManager_;
+
+                ParlayProtocolManager.openProtocol = function () {
+                    return $q(function (resolve) { resolve("success"); });
+                };
+
                 rootScope = $rootScope;
                 scope = $rootScope.$new();
-                ctrl = $controller('ParlayProtocolListController', {$scope: scope, PromenadeBroker: MockPromenadeBroker});
+                ctrl = $controller('ParlayProtocolListController', {$scope: scope, PromenadeBroker: MockPromenadeBroker, ParlayProtocolManager: ParlayProtocolManager});
             }));
             
             describe('PromenadeBroker interactions', function () {
@@ -55,6 +61,45 @@
                     expect(ctrl.isBrokerConnected()).toBeFalsy();
                     ctrl.connectBroker();
                     expect(ctrl.isBrokerConnected()).toBeTruthy();
+                });
+
+                it("gets open protocols", function () {
+                    expect(ctrl.getOpenProtocols()).toEqual([]);
+                });
+
+                it("has open protocols", function () {
+                    expect(ctrl.hasOpenProtocols()).toBeFalsy();
+                });
+
+                it("gets saved protocols", function () {
+                    expect(ctrl.getSavedProtocols()).toEqual([]);
+                });
+
+                it("has saved protocols", function () {
+                    expect(ctrl.hasSavedProtocols()).toBeFalsy();
+                });
+
+                it("opens saved protocol", function () {
+                    spyOn(ParlayProtocolManager, "openProtocol").and.callThrough();
+                    ctrl.openSavedProtocol({});
+                    expect(ParlayProtocolManager.openProtocol).toHaveBeenCalledWith({});
+                    expect(ctrl.connecting).toBeTruthy();
+                    rootScope.$apply();
+                    expect(ctrl.connecting).toBeFalsy();
+                });
+                
+                it("deletes saved protocol", function () {
+                    var configuration = {};
+                    spyOn(ParlayProtocolManager, "deleteProtocolConfiguration");
+                    ctrl.deleteSavedProtocol(configuration);
+                    expect(ParlayProtocolManager.deleteProtocolConfiguration).toHaveBeenCalledWith({});
+                });
+
+                it("closes protocol", function () {
+                    var protocol = {};
+                    spyOn(ParlayProtocolManager, "closeProtocol");
+                    ctrl.closeProtocol(protocol);
+                    expect(ParlayProtocolManager.closeProtocol).toHaveBeenCalledWith(protocol);
                 });
                 
             });

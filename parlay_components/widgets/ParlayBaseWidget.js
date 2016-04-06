1e9a633024d8bbb6601c830e15548ba3868b0139
Nick Morris <nickmorri@gmail.com>
2016-04-06 16:24:16 -0700

    feat: Add SmoothieChart for demo

--- parlay_components/widgets/ParlayBaseWidget.js
+++ parlay_components/widgets/ParlayBaseWidget.js
@@ -148,6 +148,31 @@ function ParlayDemoWidget() {
         link: function (scope, element, attributes, controller) {
             Array.prototype.slice.call(element.find("input")).forEach(controller.registerInput);
             Array.prototype.slice.call(element.find("button")).forEach(controller.registerButton);
+
+            this.smoothie = new SmoothieChart({
+                grid: {
+                    strokeStyle:'rgb(125, 0, 0)',
+                    fillStyle:'rgb(60, 0, 0)',
+                    lineWidth: 1,
+                    millisPerLine: 250,
+                    verticalSections: 6
+                },
+                labels: {
+                    fillStyle:'rgb(60, 0, 0)'
+                }
+            });
+
+            this.smoothie.streamTo(element.find("canvas")[0], 1000);
+
+            var line = new TimeSeries();
+            this.smoothie.addTimeSeries(line);
+
+            scope.$watch("transformedValue", function (newValue) {
+                if (!!newValue) {
+                    line.append(new Date().getTime(), newValue);
+                }
+            });
+
         },
         controller: function ($scope) {
 

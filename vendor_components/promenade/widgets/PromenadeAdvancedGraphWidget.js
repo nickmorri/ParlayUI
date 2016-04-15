7589211b9cf89d7d95b6176eca71f2384fca7f6b
Nick Morris <nickmorri@gmail.com>
2016-04-14 17:52:06 -0700

    feat: Integrating rickshaw

--- vendor_components/promenade/widgets/PromenadeAdvancedGraphWidget.js
+++ vendor_components/promenade/widgets/PromenadeAdvancedGraphWidget.js
@@ -13,15 +13,49 @@
         ParlayWidgetsCollection.registerWidget("promenadeAdvancedGraphWidget", "display");
     }
 
-    function PromenadeAdvancedGraphWidget() {
-        "use strict";
+    PromenadeAdvancedGraphWidget.$inject = ["$interval"];
+    function PromenadeAdvancedGraphWidget ($interval) {
         return {
             restrict: "E",
             templateUrl: "../vendor_components/promenade/widgets/directives/promenade-advanced-graph-widget.html",
-            link: function (scope) {
+            link: function (scope, element) {
 
-                scope.$watch("configuration.transformer.value", function (newValue) {
+                scope.series = [];
 
+                var graph = new Rickshaw.Graph({
+                    width: 580,
+                    height: 250,
+                    element: element.find("md-card-contents")[0],
+                    series: scope.series
+                });
+
+                function values() {
+                    return scope.configuration.transformer.items.map(function (container) {
+                        return {
+                            name: container.item.name,
+                            value: container.item.value
+                        };
+                    });
+                }
+
+                $interval(function () {
+
+                    values().forEach(function (item) {
+                        scope.series.find(function (series) {
+                            return series.name == item.name;
+                        }).data.push(item.value);
+                    });
+
+                    graph.render();
+
+                }, 1000);
+
+                scope.$watchCollection("configuration.transformer.items", function (newValue) {
+                    if (!!newValue) {
+                        scope.series = newValue.map(function (container) {
+                            return { name: container.item.name, data: [container.item.value] };
+                        });
+                    }
                 });
 
             }

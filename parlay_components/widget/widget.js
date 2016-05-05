b2e4718b6402159034006c09a9e00f5b261c184b
Nick Morris <nickmorri@gmail.com>
2016-05-05 11:52:12 -0700

    feat: Add documentation for parlay.widget

--- parlay_components/widget/widget.js
+++ parlay_components/widget/widget.js
@@ -8,16 +8,28 @@ var widgetRegistration = (function () {
         .config(ParlayWidgetsConfiguration)
         .run(ParlayWidgetsRun);
 
+    // Holds record Objects that for all widgets available to the end-user.
     var registered_widgets = [];
 
+    /**
+     * Registers the widget module and ensures that it is a dependency of parlay.widget.
+     * @param {String} module_name
+     * @param {Array} submodule_dependencies
+     * @param {String} directive_name
+     * @param {String} widget_type
+     * @param {Function} directive_function
+     */
     function widgetRegistration (module_name, submodule_dependencies, directive_name, widget_type, directive_function) {
+        // Ensure that parlay.widget includes the given module as a dependency.
         module_dependencies.push(module_name);
+
+        // Register the record Object for the given widget.
         registered_widgets.push({directive_name: directive_name, widget_type: widget_type});
 
+        // Register the widget module with AngularJS.
         angular
             .module(module_name, submodule_dependencies)
             .directive(directive_name, directive_function);
-
     }
 
     /**
@@ -40,8 +52,10 @@ var widgetRegistration = (function () {
 
     ParlayWidgetsRun.$inject = ["ParlaySettings", "ParlayWidgetCollection"];
     function ParlayWidgetsRun (ParlaySettings, ParlayWidgetCollection) {
+        // Register the default ParlaySettings for widgets.
         ParlaySettings.registerDefault("widgets", {editing: true});
 
+        // If ParlayWidget doesn't have any existing settings for widgets set to default.
         if (!ParlaySettings.has("widgets")) {
             ParlaySettings.restoreDefault("widgets");
         }

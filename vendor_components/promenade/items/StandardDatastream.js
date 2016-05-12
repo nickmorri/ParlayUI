9cbcec4ae3542e3a593a168b9495eef7c0136da7
Nick Morris <nickmorri@gmail.com>
2016-05-12 15:33:49 -0700

    feat: Use variable that references this to avoid bind
    # Conflicts:
    #	parlay/ui/vendor_components/promenade/items/StandardDatastream.js
    #	parlay/ui/vendor_components/promenade/items/StandardProperty.js

--- vendor_components/promenade/items/StandardDatastream.js
+++ vendor_components/promenade/items/StandardDatastream.js
@@ -1,48 +1,99 @@
 (function () {
     "use strict";
 
-    var module_dependencies = [];
+    var module_dependencies = ["parlay.data"];
 
     angular
         .module("promenade.items.datastream", module_dependencies)
         .factory("PromenadeStandardDatastream", PromenadeStandardDatastreamFactory);
 
-    function PromenadeStandardDatastreamFactory() {
+    PromenadeStandardDatastreamFactory.$inject = ["ParlayData", "$rootScope"];
+    function PromenadeStandardDatastreamFactory (ParlayData, $rootScope) {
 
         function PromenadeStandardDatastream(data, item_name, protocol) {
-            this.name = data.NAME;
-            this.value = undefined;
 
-            this.item_name = item_name;
-            this.protocol = protocol;
+            var datastream = this;
 
-            this.listener = protocol.onMessage({
+            datastream.type = "datastream";
+
+            datastream.name = data.NAME;
+
+            // Holds internal value in the constructor closure scope.
+            var internal_value;
+
+            // Holds callbacks that are invoked on every value change.
+            var onChangeCallbacks = {};
+
+            // defineProperty so that we can define a custom setter to allow us to do the onChange callbacks.
+            Object.defineProperty(datastream, "value", {
+                writeable: true,
+                enumerable: true,
+                get: function () {
+                    return internal_value;
+                },
+                set: function (new_value) {
+                    internal_value = new_value;
+                    Object.keys(onChangeCallbacks).forEach(function (key) {
+                        onChangeCallbacks[key](internal_value);
+                    });
+                }
+            });
+
+            datastream.item_name = item_name;
+            datastream.protocol = protocol;
+
+            datastream.listener = protocol.onMessage({
                 TX_TYPE: "DIRECT",
                 MSG_TYPE: "STREAM",
                 TO: "UI",
-                FROM: this.item_name,
-                STREAM: this.name
+                FROM: datastream.item_name,
+                STREAM: datastream.name
             }, function(response) {
-                this.value = response.VALUE;
-            }.bind(this));
+                $rootScope.$apply(function () {
+                    datastream.value = response.VALUE;
+                });
+            });
 
-            this.listen = function (stop) {
+            datastream.listen = listen;
+            datastream.onChange = onChange;
+
+            ParlayData.set(datastream.name, datastream);
+
+            /**
+             * Allows for callbacks to be registered, these will be invoked on change of value.
+             * @param {Function} callback - Function to be invoked whenever the value attribute changes.
+             * @returns {Function} - onChange deregistration function.
+             */
+            function onChange(callback) {
+                var UID = 0;
+                var keys = Object.keys(onChangeCallbacks).map(function (key) { return parseInt(key, 10); });
+                while (keys.indexOf(UID) !== -1) {
+                    UID++;
+                }
+                onChangeCallbacks[UID] = callback;
+
+                return function deregister() {
+                    delete onChangeCallbacks[UID];
+                };
+            }
+
+            function listen(stop) {
                 return protocol.sendMessage({
                         TX_TYPE: "DIRECT",
                         MSG_TYPE: "STREAM",
-                        TO: this.item_name
+                        TO: datastream.item_name
                     },
                     {
-                        STREAM: this.name,
+                        STREAM: datastream.name,
                         STOP: stop
                     },
                     {
                         TX_TYPE: "DIRECT",
                         MSG_TYPE: "STREAM",
                         TO: "UI",
-                        FROM: this.item_name
+                        FROM: datastream.item_name
                     });
-            };
+            }
 
         }
 
--- vendor_components/promenade/items/StandardProperty.js
+++ vendor_components/promenade/items/StandardProperty.js
@@ -1,63 +1,135 @@
 (function () {
     "use strict";
 
-    var module_dependencies = [];
+    var module_dependencies = ["parlay.data"];
 
     angular
         .module("promenade.items.property", module_dependencies)
         .factory("PromenadeStandardProperty", PromenadeStandardPropertyFactory);
 
-    function PromenadeStandardPropertyFactory() {
+    PromenadeStandardPropertyFactory.$inject = ["ParlayData", "$rootScope"];
+    function PromenadeStandardPropertyFactory (ParlayData, $rootScope) {
 
         function PromenadeStandardProperty(data, item_name, protocol) {
-            this.name = data.NAME;
-            this.input = data.INPUT;
-            this.read_only = data.READ_ONLY;
-            this.value = undefined;
 
-            this.item_name = item_name;
-            this.protocol = protocol;
+            var property = this;
 
-            this.get = function () {
-                return protocol.sendMessage({
-                        TX_TYPE: "DIRECT",
-                        MSG_TYPE: "PROPERTY",
-                        TO: this.item_name
-                    },
-                    {
-                        PROPERTY: this.name,
-                        ACTION: "GET",
-                        VALUE: null
-                    },
-                    {
-                        TX_TYPE: "DIRECT",
-                        MSG_TYPE: "RESPONSE",
-                        FROM: this.item_name,
-                        TO: "UI"
-                    }, true).then(function(response) {
-                    this.value = response.CONTENTS.VALUE;
-                    return response;
-                }.bind(this));
-            };
+            property.type = "property";
 
-            this.set = function () {
-                return protocol.sendMessage({
-                        TX_TYPE: "DIRECT",
-                        MSG_TYPE: "PROPERTY",
-                        TO: this.item_name
-                    },
-                    {
-                        PROPERTY: this.name,
-                        ACTION: "SET",
-                        VALUE: this.value
-                    },
-                    {
-                        TX_TYPE: "DIRECT",
-                        MSG_TYPE: "RESPONSE",
-                        FROM: this.item_name,
-                        TO: "UI"
-                    }, true);
-            };
+            property.name = data.NAME;
+            property.input = data.INPUT;
+            property.read_only = data.READ_ONLY;
+
+            // Holds internal value in the constructor closure scope.
+            var internal_value;
+
+            // Holds callbacks that are invoked on every value change.
+            var onChangeCallbacks = {};
+
+            // defineProperty so that we can define a custom setter to allow us to do the onChange callbacks.
+            Object.defineProperty(property, "value", {
+                writeable: true,
+                enumerable: true,
+                get: function () {
+                    return internal_value;
+                },
+                set: function (new_value) {
+                    internal_value = new_value;
+                    Object.keys(onChangeCallbacks).forEach(function (key) {
+                        onChangeCallbacks[key](internal_value);
+                    });
+                }
+            });
+
+            property.get = get;
+            property.set = set;
+            property.onChange = onChange;
+
+            property.item_name = item_name;
+            property.protocol = protocol;
+
+            property.listener = protocol.onMessage({
+                TX_TYPE: "DIRECT",
+                MSG_TYPE: "RESPONSE",
+                FROM: property.item_name,
+                TO: "UI"
+            }, function(response) {
+                // TODO: Talk with Daniel about refactoring property API to require property name in topics.
+                if (property.name == response.PROPERTY && response.VALUE) {
+                    $rootScope.$apply(function () {
+                        property.value = response.VALUE;
+                    });
+                }
+            });
+
+            ParlayData.set(property.name, property);
+
+            /**
+             * Allows for callbacks to be registered, these will be invoked on change of value.
+             * @param {Function} callback - Function to be invoked whenever the value attribute changes.
+             * @returns {Function} - onChange deregistration function.
+             */
+            function onChange(callback) {
+                var UID = 0;
+                var keys = Object.keys(onChangeCallbacks).map(function (key) {
+                    return parseInt(key, 10);
+                });
+                while (keys.indexOf(UID) !== -1) {
+                    UID++;
+                }
+                onChangeCallbacks[UID] = callback;
+
+                return function deregister() {
+                    delete onChangeCallbacks[UID];
+                };
+            }
+
+            function get () {
+
+                var topics = {
+                    TX_TYPE: "DIRECT",
+                    MSG_TYPE: "PROPERTY",
+                    TO: property.item_name
+                };
+                var contents = {
+                    PROPERTY: property.name,
+                    ACTION: "GET",
+                    VALUE: null
+                };
+                var response_topics = {
+                    TX_TYPE: "DIRECT",
+                    MSG_TYPE: "RESPONSE",
+                    FROM: property.item_name,
+                    TO: "UI"
+                };
+
+                return protocol.sendMessage(topics, contents, response_topics, true);
+            }
+
+            function set () {
+
+                var topics = {
+                    TX_TYPE: "DIRECT",
+                    MSG_TYPE: "PROPERTY",
+                    TO: property.item_name
+                };
+
+                var contents = {
+                    PROPERTY: property.name,
+                    ACTION: "SET",
+                    VALUE: property.value
+                };
+
+                var response_topics = {
+                    TX_TYPE: "DIRECT",
+                    MSG_TYPE: "RESPONSE",
+                    FROM: property.item_name,
+                    TO: "UI"
+                };
+
+                return protocol.sendMessage(topics, contents, response_topics, true);
+            }
+
         }
 
         return PromenadeStandardProperty;

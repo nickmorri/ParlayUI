9bc09d6fbb2370aed7c8fd99fca76237f07f7ba3
Nick Morris <nickmorri@gmail.com>
2016-05-13 16:16:33 -0700

    test: Added test suite and comments for ParlayWidgetEventHandler

--- parlay_components/widget/test/ParlayWidgetEventHandler.spec.js
+++ parlay_components/widget/test/ParlayWidgetEventHandler.spec.js
@@ -4,41 +4,65 @@
     describe("parlay.widget.eventhandler", function () {
 
         beforeEach(module("parlay.widget.eventhandler"));
+        beforeEach(module("parlay.widget.inputmanager"));
 
         describe("ParlayWidgetEventHandler", function () {
-            var ParlayInterpreter, ParlayWidgetEventHandler;
+            var scope, ParlayInterpreter, ParlayWidgetInputManager, ParlayWidgetEventHandler;
 
-            beforeEach(inject(function (_ParlayInterpreter_, _ParlayWidgetEventHandler_) {
+            beforeEach(inject(function (_$rootScope_, _ParlayInterpreter_, _ParlayWidgetInputManager_, _ParlayWidgetEventHandler_) {
+                scope = _$rootScope_.$new();
                 ParlayInterpreter = _ParlayInterpreter_;
+                ParlayWidgetInputManager = _ParlayWidgetInputManager_;
                 ParlayWidgetEventHandler = _ParlayWidgetEventHandler_;
             }));
 
             it("initializes", function () {
                 spyOn(ParlayInterpreter, "call").and.callThrough();
-
                 var handler = new ParlayWidgetEventHandler();
-
                 expect(ParlayInterpreter.call).toHaveBeenCalled();
             });
 
             it("construct", function () {
-                var event = new Event("test");
-
-                var handler = new ParlayWidgetEventHandler();
-
-                handler.construct(event);
+                (new ParlayWidgetEventHandler()).construct(new Event("test"));
             });
 
-            xit("makeEvent", function () {
+            it("attach", function () {
+                var element = document.createElement("button");
+                ParlayWidgetInputManager.registerElement("testWidget", 0, "testButton", element, scope, ['click']);
+                var event = ParlayWidgetInputManager.getEvents()[0];
                 var handler = new ParlayWidgetEventHandler();
-                var event = new Event("test");
-                handler.construct(event);
-                console.log(this.interpreter);
-                var interpreter_evt = handler.makeEvent(this.interpreter, event);
+                handler.attach(event);
+                expect(event.handler).toBe(handler);
             });
 
-            xit("attachEvent", function () {
+            it("detach", function () {
+                var element = document.createElement("button");
+                ParlayWidgetInputManager.registerElement("testWidget", 0, "testButton", element, scope, ['click']);
+                var event = ParlayWidgetInputManager.getEvents()[0];
 
+                var handler = new ParlayWidgetEventHandler();
+                handler.attach(event);
+                expect(event.handler).toBe(handler);
+                handler.detach();
+                expect(event.handler).toBe(null);
+            });
+
+            it("attachEvent", function () {
+                spyOn(window.console, "log");
+                var element = document.createElement("input");
+                ParlayWidgetInputManager.registerElement("testWidget", 0, "testInput", element, scope, ['click']);
+                var event = ParlayWidgetInputManager.getEvents()[0];
+
+                var handler = new ParlayWidgetEventHandler();
+                handler.functionString = "log('test')";
+                handler.attach(event);
+
+                element.dispatchEvent(new Event('click', {
+                    'view': window,
+                    'bubbles': true,
+                    'cancelable': true
+                }));
+                expect(window.console.log).toHaveBeenCalledWith('test');
             });
 
             it("toJSON", function () {

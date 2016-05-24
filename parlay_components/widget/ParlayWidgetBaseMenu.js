d4ffc692f017f1c410e6ddbc6a189462ef7acec5
Nick Morris <nickmorri@gmail.com>
2016-05-24 11:13:57 -0700

    doc: Added docstring

--- parlay_components/widget/ParlayWidgetBaseMenu.js
+++ parlay_components/widget/ParlayWidgetBaseMenu.js
@@ -6,6 +6,19 @@
     angular
         .module("parlay.widget.base.menu", module_dependencies)
         .directive("parlayWidgetBaseMenu", ParlayWidgetBaseMenu);
+
+    /**
+     * @directive
+     * @name ParlayWidgetBaseMenu
+     *
+     * @description
+     * ParlayWidgetBaseMenu directive that contains a typical <md-menu> element used in a ParlayWidget.
+     *
+     * @example
+     *
+     *  <parlay-widget-base-menu></parlay-widget-base-menu>
+     *
+     */
     
     function ParlayWidgetBaseMenu () {
         return {

0b27b6fcab45af4ad55d3fbdde25982212047d1f
Nick Morris <nickmorri@gmail.com>
2016-05-25 13:57:38 -0700

    feat: Simplify ParlayData to use a JavaScript Map()

--- parlay_components/utility/ParlayData.js
+++ parlay_components/utility/ParlayData.js
@@ -1,48 +1,11 @@
-function ParlayDataFactory() {
+(function () {
     "use strict";
 
-    var container = new Map();
+    // Simple wrapper for a JavaScript Map.
 
-    function ParlayData() {}
+    angular.module("parlay.data", [])
+        .factory("ParlayData", function () {
+            return new Map();
+        });
 
-    ParlayData.prototype.clear = function () {
-        container.clear();
-    };
-
-    ParlayData.prototype.delete = function (key) {
-        return container.delete(key);
-    };
-
-    ParlayData.prototype.entries = function () {
-        return container.entries();
-    };
-
-    ParlayData.prototype.forEach = function (callbackFn, thisArg) {
-        container.forEach(callbackFn, thisArg);
-    };
-
-    ParlayData.prototype.get = function (key) {
-        return container.get(key);
-    };
-
-    ParlayData.prototype.has = function (key) {
-        return container.has(key);
-    };
-
-    ParlayData.prototype.keys = function () {
-        return container.keys();
-    };
-
-    ParlayData.prototype.set = function (key, value) {
-        container.set(key, value);
-    };
-
-    ParlayData.prototype.values = function () {
-        return container.values();
-    };
-
-    return new ParlayData();
-}
-
-angular.module("parlay.data", [])
-    .factory("ParlayData", [ParlayDataFactory]);
+}());

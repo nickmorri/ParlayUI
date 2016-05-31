3eb16f5425696193421a7b44365885c6dd0f3b02
Nick Morris <nickmorri@gmail.com>
2016-05-31 16:40:55 -0700

    feat: Settings dialog has restore defaults options

--- parlay_components/settings/ParlaySettingsDialog.js
+++ parlay_components/settings/ParlaySettingsDialog.js
@@ -30,15 +30,10 @@
 
         // Attach $mdDialog.hide to controller to allow user to dismiss dialog.
         ctrl.hide = $mdDialog.hide;
+        ctrl.restoreDefault = restoreDefault;
 
-        var log_settings = ParlaySettings.get("log");
-        var graph_settings = ParlaySettings.get("graph");
-        var broker_settings = ParlaySettings.get("broker");
-
-        $scope.auto_discovery = broker_settings && broker_settings.auto_discovery;
-        $scope.max_log_size = log_settings && parseInt(log_settings.max_size);
-        $scope.label_size = graph_settings && parseInt(graph_settings.label_size);
-        $scope.show_prompt = broker_settings && broker_settings.show_prompt;
+        var log_settings, graph_settings, broker_settings;
+        loadStaticSettingValues();
 
         $scope.notification_permission = !navigator.userAgent.includes("Edge") && Notification.permission === "granted";
 
@@ -57,6 +52,22 @@
         $scope.$watch("show_prompt", function (newValue) {
             ParlaySettings.set("broker", {show_prompt: newValue});
         });
+
+        function loadStaticSettingValues () {
+            log_settings = ParlaySettings.get("log");
+            graph_settings = ParlaySettings.get("graph");
+            broker_settings = ParlaySettings.get("broker");
+
+            $scope.auto_discovery = broker_settings && broker_settings.auto_discovery;
+            $scope.max_log_size = log_settings && parseInt(log_settings.max_size);
+            $scope.label_size = graph_settings && parseInt(graph_settings.label_size);
+            $scope.show_prompt = broker_settings && broker_settings.show_prompt;
+        }
+
+        function restoreDefault (setting) {
+            ParlaySettings.restoreDefault(setting);
+            loadStaticSettingValues();
+        }
 
         /**
          * Download the last discovery information as a text file.

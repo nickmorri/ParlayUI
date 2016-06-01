5b6b34be94a367f30d5f7734ea058d52017c267b
Nick Morris <nickmorri@gmail.com>
2016-06-01 13:25:18 -0700

    refactor: Reorganizing ParlayNavigationSidenavController

--- parlay_components/navigation/ParlayNavigationSidenav.js
+++ parlay_components/navigation/ParlayNavigationSidenav.js
@@ -6,35 +6,115 @@
     angular.module("parlay.navigation.sidenav", module_dependencies)
         .controller("ParlayNavigationSidenavController", ParlayNavigationSidenavController);
 
-    /* istanbul ignore next */
     ParlayNavigationSidenavController.$inject = ["$mdSidenav", "$mdDialog", "ParlayGenericSaveLoadDialog", "$state", "PromenadeBroker", "ParlayItemManager", "ParlayWidgetManager"];
     /**
      * Controller for the navigation $mdSidenav.
      * @constructor module:ParlayNavigation.ParlayNavigationSidenavController
      * @param {Object} $mdSidenav - Angular Material service.
      * @param {Object} $mdDialog - Angular Material service.
-     * @param {Object} ParlayGenericSaveLoadDialog
+     * @param {Object} ParlayGenericSaveLoadDialog - ParlayGenericSaveLoadDialog service.
      * @param {Object} $state - ui.router $stateProvider service.
      * @param {Object} PromenadeBroker - PromenadeBroker service.
      * @param {Object} ParlayItemManager - ParlayItemManager service.
+     * @param {Object} ParlayWidgetManager - ParlayWidgetManager service.
      */
     function ParlayNavigationSidenavController($mdSidenav, $mdDialog, ParlayGenericSaveLoadDialog, $state, PromenadeBroker, ParlayItemManager, ParlayWidgetManager) {
 
-        this.getCurrentState = function () {
-            return $state.current;
-        };
+        var ctrl = this;
 
-        this.getAllStates = function () {
+        // Attach methods to controller.
+        ctrl.getCurrentState = getCurrentState;
+        ctrl.getAllStates = getAllStates;
+        ctrl.navigateState = navigateState;
+        ctrl.openProtocolManagementDialog = openProtocolManagementDialog;
+        ctrl.requestDiscovery = requestDiscovery;
+        ctrl.openNotificationSidenav = openNotificationSidenav;
+        ctrl.openHelpTab = openHelpTab;
+        ctrl.openSettingsDialog = openSettingsDialog;
+        ctrl.requestDiscovery = requestDiscovery;
+        ctrl.openItemSaveLoadDialog = openItemSaveLoadDialog;
+        ctrl.toggleWidgetEditing = toggleWidgetEditing;
+        ctrl.openWidgetSaveLoadDialog = openWidgetSaveLoadDialog;
+
+        /**
+         * Reference the editing state of the ParlayWidgetManager.
+         * @member module:ParlayNavigation.ParlayNavigationSidenavController#editing
+         * @public
+         * @type {Boolean}
+         */
+        ctrl.editing = ParlayWidgetManager.editing;
+
+        /**
+         * Returns the current state Object from the ui.router $stateProvider.
+         * @member module:ParlayNavigation.ParlayNavigationSidenavController#getCurrentState
+         * @public
+         * @returns {Object}
+         */
+        function getCurrentState () {
+            return $state.current;
+        }
+
+        /**
+         * Returns the all, non-abstract, state Objects from the ui.router $stateProvider.
+         * @member module:ParlayNavigation.ParlayNavigationSidenavController#getAllStates
+         * @public
+         * @returns {Array}
+         */
+        function getAllStates () {
             return $state.get().filter(function (state) {
                 return !state.abstract;
             });
-        };
+        }
 
-        this.navigateState = function (state) {
+        /**
+         * Request the ui.router $stateProvider to go to the given state.
+         * @member module:ParlayNavigation.ParlayNavigationSidenavController#navigateState
+         * @public
+         * @param {Object} state - ui.router state Object to go to.
+         * @returns {Array}
+         */
+        function navigateState (state) {
             $state.go(state);
-        };
+        }
 
-        this.openProtocolManagementDialog = function (event) {
+        function toggleWidgetEditing  () {
+            ParlayWidgetManager.toggleEditing();
+        }
+
+        /**
+         * Requests PromenadeBroker performs a discovery.
+         * @member module:ParlayNavigation.ParlayNavigationSidenavController#requestDiscovery
+         * @public
+         */
+        function requestDiscovery () {
+            PromenadeBroker.requestDiscovery(true);
+        }
+
+        /**
+         * Opens the notifications $mdSidenav.
+         * @member module:ParlayNavigation.ParlayNavigationSidenavController#openNotificationSidenav
+         * @public
+         */
+        function openNotificationSidenav () {
+            $mdSidenav("notifications").open();
+        }
+
+        /**
+         * Launches a new tab with the Parlay documentation.
+         * @member module:ParlayNavigation.ParlayNavigationSidenavController#openNotificationSidenav
+         * @public
+         */
+        function openHelpTab () {
+            window.open(window.location.origin + "/docs", '_blank').focus();
+        }
+
+        /**
+         * Launches a $mdDialog for the Parlay protocol list.
+         * @member module:ParlayNavigation.ParlayNavigationSidenavController#openProtocolManagementDialog
+         * @public
+         * @param {Event} event - DOM Event that can be used to set an animation source for $mdDialog.
+         */
+        function openProtocolManagementDialog (event) {
             $mdDialog.show({
                 templateUrl: "../parlay_components/protocols/directives/parlay-protocol-list-dialog.html",
                 targetEvent: event,
@@ -42,31 +122,15 @@
                 controllerAs: "ctrl",
                 clickOutsideToClose: true
             });
-        };
+        }
 
-        this.requestDiscovery = function () {
-            PromenadeBroker.requestDiscovery(true);
-        };
-
-        this.openWorkspaceManagementDialog = function (event) {
-            (new ParlayGenericSaveLoadDialog(event, ParlayItemManager, {
-                entry: "workspace",
-                entries: "workspaces",
-                title: "workspaces",
-                child: "item",
-                children: "items"
-            }));
-        };
-
-        this.openNotificationSidenav = function () {
-            $mdSidenav("notifications").open();
-        };
-
-        this.openHelpTab = function () {
-            window.open(window.location.origin + "/docs", '_blank').focus();
-        };
-
-        this.openSettingsDialog = function (event) {
+        /**
+         * Launches a $mdDialog for Parlay settings.
+         * @member module:ParlayNavigation.ParlayNavigationSidenavController#openSettingsDialog
+         * @public
+         * @param {Event} event - DOM Event that can be used to set an animation source for $mdDialog.
+         */
+        function openSettingsDialog (event) {
             $mdDialog.show({
                 templateUrl: "../parlay_components/settings/directives/parlay-settings-dialog.html",
                 targetEvent: event,
@@ -74,31 +138,31 @@
                 controllerAs: "ctrl",
                 clickOutsideToClose: true
             });
-        };
+        }
 
-        // The below methods are only appropriate based on the given state.
-
-        this.requestDiscovery = function () {
-            PromenadeBroker.requestDiscovery(true);
-        };
-
-        this.openItemSaveLoadDialog = function (event) {
-            (new ParlayGenericSaveLoadDialog(event, ParlayItemManager, {
+        /**
+         * Launches a ParlayGenericSaveLoadDialog for item workspace management.
+         * @member module:ParlayNavigation.ParlayNavigationSidenavController#openWorkspaceManagementDialog
+         * @public
+         * @param {Event} event - DOM Event that can be used to set an animation source for $mdDialog.
+         */
+        function openItemSaveLoadDialog (event) {
+            ParlayGenericSaveLoadDialog.show(event, ParlayItemManager, {
                 entry: "workspace",
                 entries: "workspaces",
                 title: "workspaces",
                 child: "item",
                 children: "items"
-            }));
-        };
+            });
+        }
 
-        this.editing = ParlayWidgetManager.editing;
-
-        this.toggleWidgetEditing = function () {
-            ParlayWidgetManager.toggleEditing();
-        };
-
-        this.openWidgetSaveLoadDialog = function (event) {
+        /**
+         * Launches a ParlayGenericSaveLoadDialog for widgets workspace management.
+         * @member module:ParlayNavigation.ParlayNavigationSidenavController#openWidgetSaveLoadDialog
+         * @public
+         * @param {Event} event - DOM Event that can be used to set an animation source for $mdDialog.
+         */
+        function openWidgetSaveLoadDialog (event) {
             ParlayGenericSaveLoadDialog.show(event, ParlayWidgetManager, {
                 entry: "workspace",
                 entries: "workspaces",
@@ -106,7 +170,7 @@
                 child: "widget",
                 children: "widgets"
             });
-        };
+        }
 
     }
 
